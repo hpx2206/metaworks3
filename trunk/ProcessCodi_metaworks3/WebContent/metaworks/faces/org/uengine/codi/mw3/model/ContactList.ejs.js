7e250c9cff0b2d16712fffee249caaccbf20be6a
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-01-27 03:47:10 +0000

    팝업 기능 수정 및 적용(사용자 추가, 팔로워 추가)

--- trunk/ProcessCodi_metaworks3/WebContent/metaworks/faces/org/uengine/codi/mw3/model/ContactList.ejs.js
+++ trunk/ProcessCodi_metaworks3/WebContent/metaworks/faces/org/uengine/codi/mw3/model/ContactList.ejs.js
@@ -4,3 +4,4 @@ var org_uengine_codi_mw3_model_ContactList = function(objectId, className) {
 	
 	console.debug(mw3.objects[objectId]);
 }
+
---
+++ trunk/ProcessCodi_metaworks3/WebContent/metaworks/faces/org/uengine/codi/mw3/model/Followers.ejs.js
@@ -0,0 +1,8 @@
+function addFollowers(objectId, e){
+	var object = mw3.getObject(objectId);	
+	
+	object.pageX = e.pageX;
+	object.pageY = e.pageY;			
+
+	mw3.call(objectId, "addFollowers");	
+}
