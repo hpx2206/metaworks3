bb85ff7f453347f67e2c064c2f9a2b8fda3c1085
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-01-30 10:04:02 +0000

    svn update 후 application 오류 해결

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/ClassDesignerContentPanel.java
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/ClassDesignerContentPanel.java
@@ -1,24 +1,15 @@
 package org.uengine.codi.mw3.model;
 
-import java.rmi.RemoteException;
-import java.util.ArrayList;
-
 import org.metaworks.annotation.Face;
-import org.metaworks.example.ide.CodeAssist;
-import org.metaworks.example.ide.CodeAssister;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.uengine.codi.mw3.admin.ClassDefinition;
-import org.uengine.codi.mw3.admin.ClassField;
-import org.uengine.codi.mw3.admin.FormDefinition;
-import org.uengine.codi.mw3.admin.FormInstance;
-import org.uengine.codi.mw3.model.ContentPanel;
 import org.uengine.kernel.GlobalContext;
 import org.uengine.processmanager.ProcessManagerRemote;
 
 @Face(ejsPath="genericfaces/Window.ejs", displayName="Class Designer", options={"hideLabels"}, values={"true"})
 public class ClassDesignerContentPanel extends ContentWindow {
 	
-
+	
 	ClassDefinition classDefinition;
 		public ClassDefinition getClassDefinition() {
 			return classDefinition;
