8c5ee9b73d26b9e005ba075671338a1b4e4bdf46
somehow <somehow@uengine.org>
2012-01-30 10:21:11 +0000

    number 형 추가

---
+++ mw3/WebContent/metaworks/faces/java/lang/Number.ejs.js
@@ -0,0 +1,17 @@
+function onlyNumberInput() {
+	
+	 var e = window.event;
+
+	 if (e.keyCode == 8 || e.keyCode == 9 || e.keyCode == 13) {
+		 return true;
+	 } else if (e.keyCode >= 33 && e.keyCode <= 46) {
+		 return true;
+	 } else if((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)){
+    	 e.returnValue = true;
+     } else { 
+    	 window.event.returnValue = false;
+    	 
+    	 return false;
+     }
+	 
+}
