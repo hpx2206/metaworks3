e53e67d5fc36c755c814994fb74d090d6c821e1b
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-01-31 05:18:32 +0000

    MobileWindow.ejs, MobileWindow.ejs.sj 추가 (ClassDesigner 의 runMobile 용)

---
+++ trunk/ProcessCodi_metaworks3/WebContent/metaworks/genericfaces/MobileWindow.ejs.js
@@ -0,0 +1,7 @@
+var MobileWindow = function(objectId, className){
+	this.objectId = objectId;
+	this.className = className;
+	
+	console.debug("mw3.mouseX : " + mw3.mouseX);
+	console.debug("mw3.mouseY : " + mw3.mouseY);
+} 
