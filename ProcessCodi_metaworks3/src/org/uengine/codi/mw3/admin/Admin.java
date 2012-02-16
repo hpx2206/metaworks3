8a1fd79918658f64750699bfb96baea29cf45b80
somehow <somehow@uengine.org>
2012-02-16 08:27:48 +0000

    바로 로그인 후 admin 으로

--- ProcessCodi_metaworks3/src/org/uengine/codi/mw3/admin/Admin.java
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/admin/Admin.java
@@ -1,12 +1,19 @@
 package org.uengine.codi.mw3.admin;
 
+import javax.servlet.http.HttpSession;
+
+import org.metaworks.annotation.ServiceMethod;
+import org.metaworks.dao.TransactionContext;
 import org.uengine.codi.mw3.model.ContentWindow;
 import org.uengine.codi.mw3.model.ILogin;
+import org.uengine.codi.mw3.model.Login;
 import org.uengine.codi.mw3.model.Session;
 
 
 public class Admin {
 	
+	public Admin(){}
+			
 	public Admin(ILogin login) throws Exception{
 //		formDefinition = new FormDefinition();
 //		formInstance = new FormInstance();
@@ -18,6 +25,24 @@ public class Admin {
 		this.session = session;
 
 		contentWindow = new ContentWindow();
+	}
+	
+	@ServiceMethod
+	public void load() throws Exception{
+		//setting the facebook user Id into session attribute;
+		HttpSession httpSession = TransactionContext.getThreadLocalInstance().getRequest().getSession(); 
+		String userId = (String)httpSession.getAttribute("userId");
+		
+		Login login = new Login();
+		login.setUserId(userId);
+		
+		resourceWindow = new ResourceWindow();
+
+		Session session = new Session();
+		session.setLogin(login);
+		this.session = session;
+
+		contentWindow = new ContentWindow();		
 	}
 	
 	ResourceWindow resourceWindow;
@@ -43,5 +68,4 @@ public class Admin {
 		public void setSession(Session session) {
 			this.session = session;
 		}
-
 }
