7e326f3440a2a6234c3434d6052fe5150c63bfdf
somehow <somehow@uengine.org>
2012-02-17 17:34:43 +0000

    workflowy 수정

--- ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/Workflowy.java
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/Workflowy.java
@@ -4,27 +4,14 @@ import java.net.UnknownHostException;
 
 import org.metaworks.ContextAware;
 import org.metaworks.MetaworksContext;
+import org.metaworks.annotation.Face;
+import org.metaworks.annotation.ServiceMethod;
 
-import com.mongodb.DB;
-import com.mongodb.Mongo;
 import com.mongodb.MongoException;
 
 public class Workflowy implements ContextAware {
-	
-	public Workflowy() throws UnknownHostException, MongoException{
-		node = new WorkflowyNode(1);
-		node.setName("ROOT");				
-		node.getMetaworksContext().setWhen(MetaworksContext.WHEN_VIEW);
-		node.getMetaworksContext().setWhere("ROOT");
-		node.load();
-		
-		if(node.getChildNode().size() == 0)
-			node.newNode();
-		
-		setNode(node);
-	}
-	
-	transient MetaworksContext metaworksContext;
+			
+	MetaworksContext metaworksContext;
 		public MetaworksContext getMetaworksContext() {
 			return metaworksContext;
 		}
@@ -40,4 +27,19 @@ public class Workflowy implements ContextAware {
 		public void setNode(WorkflowyNode node) {
 			this.node = node;
 		}
+		
+	@ServiceMethod
+    @Face(displayName="시작하기")
+	public void load() throws UnknownHostException, MongoException {
+		node = new WorkflowyNode(1);
+		node.setName("ROOT");				
+		node.getMetaworksContext().setWhen(MetaworksContext.WHEN_VIEW);
+		node.getMetaworksContext().setWhere("ROOT");
+		node.load();
+		
+		if(node.getChildNode().size() == 0)
+			node.newNode();
+		
+		setNode(node);		
+	}
 }
