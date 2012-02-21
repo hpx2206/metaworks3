0ac11c61e6d603c86dc9eae05d2efe30223e4b89
somehow <somehow@uengine.org>
2012-02-21 08:04:27 +0000

    facebook 관련 수정

--- ProcessCodi_metaworks3/WebContent/metaworks/faces/org/uengine/codi/mw3/model/ILogin.ejs.js
+++ ProcessCodi_metaworks3/WebContent/metaworks/faces/org/uengine/codi/mw3/model/ILogin.ejs.js
@@ -4,29 +4,35 @@ var org_uengine_codi_mw3_model_ILogin = function(objectId, className){
 	this.className = className;
 	var theLoginHelper = this;
 	
-    window.fbAsyncInit = function() {
-      FB.init({
-        appId      : '119159701538006',// 296566710367686   119159701538006
-        status     : true, 
-        cookie     : true,
-        xfbml      : true
-      });
-      
-  	FB.login(function(response) {
-		   if (response.authResponse) {
-			   theLoginHelper.setUserInfo();
-		   } else {
-		     console.log('User cancelled login or did not fully authorize.');
-		   }
-		 }, {scope: 'email,user_checkins,publish_stream,user_likes,export_stream'});
-	
-    };
-    (function(d){
-       var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-       js = d.createElement('script'); js.id = id; js.async = true;
-       js.src = "//connect.facebook.net/en_US/all.js";
-       d.getElementsByTagName('head')[0].appendChild(js);
-     }(document));
+	window.fbAsyncInit = function() {
+		FB.init({
+			     appId  : '119159701538006',
+			     status : true, // check login status
+			     cookie : true, // enable cookies to allow the server to access the session
+			     xfbml  : true  // parse XFBML
+			   });	
+		
+		FB.getLoginStatus(function(response) {
+			if (response.status === 'connected') {		
+			    // the user is logged in and connected to your
+			    // app, and response.authResponse supplies
+			    // the user's ID, a valid access token, a signed
+			    // request, and the time the access token 
+			    // and signed request each expire
+			    var uid = response.authResponse.userID;
+			    var accessToken = response.authResponse.accessToken;
+			    
+			    theLoginHelper.setUserInfo();
+			  } else if (response.status === 'not_authorized') {
+			    // the user is logged in to Facebook, 
+			    //but not connected to the app
+			  } else {
+				  $("#fb-login").show();
+
+			  }
+		});
+	};    
+    
     
    
 //	var tempUsr = {
