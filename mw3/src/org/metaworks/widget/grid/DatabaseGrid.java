d2be5c4f620fef9f0da5068d2394011c83eadf98
somehow <somehow@uengine.org>
2012-03-05 13:43:18 +0000

    grid 수정 (edit 시 database update)

--- mw3/src/org/metaworks/widget/grid/DatabaseGrid.java
+++ mw3/src/org/metaworks/widget/grid/DatabaseGrid.java
@@ -1,17 +1,40 @@
 package org.metaworks.widget.grid;
 
+import org.metaworks.annotation.ServiceMethod;
+import org.metaworks.dao.Database;
 import org.metaworks.dao.IDAO;
 
 
 public class DatabaseGrid extends ObjectGrid{
-
+	
+	
 	public DatabaseGrid() throws Exception {
 		super();
 	}
+	
+	
 
 	public void setObjectData(Class dataClass, IDAO dao) throws Exception {
 		
 		setObjectType(dataClass);
 		setData(dao);
+	}
+	
+	@Override
+	@ServiceMethod(callByContent = true, target="none")
+	public void changeCell() {
+		
+		try {
+			Class daoClass = Thread.currentThread().getContextClassLoader().loadClass(getClassName());
+						
+			IDAO dao = Database.get(daoClass, getCell().getRowId());
+			
+			dao.set(getCell().getName(), getCell().getValue());
+			
+			throw new Exception("error");
+			
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
