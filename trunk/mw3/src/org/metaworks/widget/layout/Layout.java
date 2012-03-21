0cc9f21c6667c00093c8610749aae9a5c7060532
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-03-21 14:00:13 +0000

    uclipse 관련 수정

---
+++ trunk/mw3/src/org/metaworks/widget/layout/Layout.java
@@ -0,0 +1,73 @@
+package org.metaworks.widget.layout;
+
+public class Layout {
+
+	public Layout() {
+		setLoad(true);
+		setLoadChild(false);		
+	}
+	
+	Object north;
+		public Object getNorth() {
+			return north;
+		}	
+		public void setNorth(Object north) {
+			this.north = north;
+		}
+		
+	Object west;
+		public Object getWest() {
+			return west;
+		}	
+		public void setWest(Object west) {
+			this.west = west;
+		}
+	
+	Object center;
+		public Object getCenter() {
+			return center;
+		}	
+		public void setCenter(Object center) {
+			this.center = center;
+		}
+		
+	Object east;
+		public Object getEast() {
+			return east;
+		}	
+		public void setEast(Object east) {
+			this.east = east;
+		}	
+		
+	Object south;
+		public Object getSouth() {
+			return south;
+		}	
+		public void setSouth(Object south) {
+			this.south = south;
+		}
+		
+	String options;
+		public String getOptions() {
+			return options;
+		}
+		public void setOptions(String options) {
+			this.options = options;
+		}
+		
+	boolean load;	
+		public boolean isLoad() {
+			return load;
+		}
+		public void setLoad(boolean load) {
+			this.load = load;
+		}
+		
+	boolean loadChild;
+		public boolean isLoadChild() {
+			return loadChild;
+		}
+		public void setLoadChild(boolean loadChild) {
+			this.loadChild = loadChild;
+		}
+}
