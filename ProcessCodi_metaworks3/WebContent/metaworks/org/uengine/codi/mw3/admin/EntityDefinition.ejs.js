b3cc68186a6317fb0f1ac79b2ab661592d12a4f8
somehow <somehow@uengine.org>
2012-03-22 02:16:02 +0000

    uclipse 관련 수정

--- ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/admin/EntityDefinition.ejs.js
+++ ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/admin/EntityDefinition.ejs.js
@@ -1,8 +1,15 @@
 var org_uengine_codi_mw3_admin_EntityDefinition = function(objectId, className){
 	this.objectId = objectId;
 	this.className = className;
-	
+
 	var object = mw3.objects[this.objectId];
+	
+	if(object.entityName != null){
+		$(".mw3_window").each(function(){
+			if($(this).attr("className") == "org.uengine.codi.mw3.admin.ContentWindow")
+				$(this).find("#navigationTitle").html(object.entityName + ".sql");
+		});
+	}
 	
 	// overrides the function
 	object.generateDDL = function() {
@@ -12,15 +19,32 @@ var org_uengine_codi_mw3_admin_EntityDefinition = function(objectId, className){
 			
 			return false;
 		}
-		
+
 		if(object.entityFields == null){
 			alert("'Entity Field' 을 먼저 추가하세요.");
 			
 			return false;
 		}
-		
+
 		$("#objDiv_" + objectId).parentsUntil("mw3_tab").tabs("select",1);
 		
 		mw3.call(objectId, 'generateDDL');
 	}
+
+	object.generateDao = function() {
+		
+		if(object.parentFolder == null){
+			object.parentFolder = "1";
+		}
+		
+		mw3.call(objectId, 'generateDao');
+		
+		var importValue = 'import ' + object.packageName + '.' + object.entityName + ';';
+		importValue = importValue + '\n' + 'import ' + object.packageName + '.I' + object.entityName + ';';
+		
+		var codeAssist = mw3.getAutowiredObject("org.metaworks.example.ide.CodeAssist");
+		codeAssist.extendImportValue = importValue;
+		codeAssist.ExtendImport();
+		
+	}	
 }
