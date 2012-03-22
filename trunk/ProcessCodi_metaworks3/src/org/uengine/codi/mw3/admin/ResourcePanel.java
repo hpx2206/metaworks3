d193e738fc946256c2c5acb78c3be0b1e1d18b7e
jyjang@uengine.org <jyjang@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-03-22 05:35:35 +0000

    File based Process Definition Loading & Class path system

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/admin/ResourcePanel.java
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/admin/ResourcePanel.java
@@ -1,34 +1,46 @@
 package org.uengine.codi.mw3.admin;
 
+import org.metaworks.MetaworksContext;
+import org.metaworks.annotation.AutowiredFromClient;
+import org.metaworks.annotation.AutowiredToClient;
+import org.metaworks.annotation.Face;
 import org.metaworks.annotation.ServiceMethod;
 import org.metaworks.dwr.MetaworksRemoteService;
 import org.uengine.codi.mw3.model.IProcessDefinition;
 import org.uengine.codi.mw3.model.ProcessDefinition;
+import org.uengine.codi.mw3.model.ResourceFile;
+import org.uengine.codi.mw3.model.SelectedProcessDefinition;
 
 public class ResourcePanel {
 	
 	@ServiceMethod
 	public void refresh() throws Exception {
 		
-		ProcessDefinition root = new ProcessDefinition();
-		root.setParentFolder(new Long(-1));
-		processDefinitions = root.findAll();
+		resourceFile = new ResourceFile();
+		resourceFile.setMetaworksContext(new MetaworksContext());	
+		resourceFile.getMetaworksContext().setWhere("design");
+		resourceFile.getMetaworksContext().setWhen("design");
+
+		resourceFile.setFolder(true);
+		resourceFile.setAlias("");
+		resourceFile.setName("/");
+		resourceFile.drillDown();
 		
-		processDefinitions.getMetaworksContext().setWhere("design");
 		
 		MetaworksRemoteService.getInstance().clearMetaworksType("*");
 	}
 	
-	IProcessDefinition processDefinitions;
-	
-		public IProcessDefinition getProcessDefinitions() {
-			return processDefinitions;
+
+	ResourceFile resourceFile;
+
+
+		public ResourceFile getResourceFile() {
+			return resourceFile;
 		}
 	
-		public void setProcessDefinitions(IProcessDefinition processDefinitions) {
-			this.processDefinitions = processDefinitions;
-		}
-		
 	
+		public void setResourceFile(ResourceFile resourceFile) {
+			this.resourceFile = resourceFile;
+		}
 
 }
