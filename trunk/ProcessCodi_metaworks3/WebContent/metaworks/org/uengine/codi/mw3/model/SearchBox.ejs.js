1562c2e304ea8664e845b1a08267f69a7ba30e03
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-03-22 02:17:13 +0000

    uclipse 관련 수정

--- trunk/ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/model/SearchBox.ejs.js
+++ trunk/ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/model/SearchBox.ejs.js
@@ -1,7 +1,10 @@
+//var timeout;
+
 var org_uengine_codi_mw3_model_SearchBox = function(objectId, className) {
 	this.objectId = objectId;
 	this.className = className;
-
+	this.timeout;
+	
 	$("#search_" + this.objectId).focus();
 }
 
@@ -13,17 +16,28 @@ org_uengine_codi_mw3_model_SearchBox.prototype.getValue = function() {
 	return object;
 }
 
-var threadSearch = null;
-
-function keyEventProcess(objectId) {
-	if (this.threadSearch != null) {
-		clearTimeout(this.threadSearch);
+org_uengine_codi_mw3_model_SearchBox.prototype.keydown = function() {
+	var objectId = this.objectId;
+	
+	console.debug(this.timeout);
+	
+	if (this.timeout) {
+		clearTimeout(this.timeout);
 	}
-
-	this.threadSearch = setTimeout(function() {
-		//$("#search_" + objectId).blur();
-
-		var value = mw3.getObject(objectId);
-		value.search();
+	
+	this.timeout = setTimeout(function() {
+		console.debug("search");
+		
+		mw3.call(objectId, 'search');
 	}, 500);
+	
+}
+
+org_uengine_codi_mw3_model_SearchBox.prototype.newInstance = function(){
+	
+	var instanceListPanel = mw3.getAutowiredObject("org.uengine.codi.mw3.model.InstanceListPanel");
+	if(instanceListPanel){
+		instanceListPanel.newInstance();
+	}
+	
 }
