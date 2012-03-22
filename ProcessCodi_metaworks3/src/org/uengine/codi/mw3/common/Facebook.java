0f6b053f8d2515661d0045936f8ef3ff83e4ac82
somehow <somehow@uengine.org>
2012-03-22 02:11:55 +0000

    uclipse 관련 수정

---
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/common/Facebook.java
@@ -0,0 +1,12 @@
+package org.uengine.codi.mw3.common;
+
+public class Facebook {
+
+	String url;
+		public String getUrl() {
+			return url;
+		}
+		public void setUrl(String url) {
+			this.url = url;
+		}
+}
