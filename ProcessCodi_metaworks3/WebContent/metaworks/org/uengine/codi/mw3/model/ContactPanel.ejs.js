61c343986fe32a1f1ce8fd6f655e03d8f94497dc
somehow <somehow@uengine.org>
2012-03-28 07:58:11 +0000

    ide 관련 수정

--- ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/model/ContactPanel.ejs.js
+++ ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/model/ContactPanel.ejs.js
@@ -2,19 +2,47 @@ var org_uengine_codi_mw3_model_ContactPanel = function(objectId, className) {
 	this.objectId = objectId;
 	this.className = className;
 
-	this.divId = "objDiv_" + objectId;	
-	$("#" + this.divId).css("height","100%");
+	this.divId = '#objDiv_' + objectId;	
 	
-	var layout = $("#" + this.divId).closest(".mw3_window").parent();
 	
-	var height = layout.css("padding-bottom");
-	if(height.length){
-		height = height.replace('px', '');
-		
-		height = Number(height);
-		
-		layout.css("padding-bottom", String(height + 100) + 'px');
-		
+	$(this.divId).addClass('mw3_layout').attr('objecctId', objectId);
+	
+	var faceHelper = this;
+	
+	faceHelper.load();
+}
+
+org_uengine_codi_mw3_model_ContactPanel.prototype.load = function(){
+	
+	var object = mw3.objects[this.objectId];
+	var options = {
+			togglerLength_open:	0, 
+			spacing_open:		0, 
+			spacing_closed:		0,
+			center__onresize:	'mw3.getFaceHelper('+this.objectId+').resizeChild()'
 	}
+
+	this.layout = $(this.divId).layout(options);
+}
+
+org_uengine_codi_mw3_model_ContactPanel.prototype.destory = function(){
+	$(this.divId).layout().destroy();
+}
+
+org_uengine_codi_mw3_model_ContactPanel.prototype.resize = function(){
+	if(this.layout){
+		this.layout.resizeAll();
+		
+		this.resizeChild();
+	}
+}
+
+org_uengine_codi_mw3_model_ContactPanel.prototype.resizeChild = function(){
 	
+	$(this.divId).find('.mw3_layout').each(function(){
+		var layoutId = $(this).attr('objectId');
+		
+		if(layoutId)
+			mw3.getFaceHelper(layoutId).resize();
+	});
 }
