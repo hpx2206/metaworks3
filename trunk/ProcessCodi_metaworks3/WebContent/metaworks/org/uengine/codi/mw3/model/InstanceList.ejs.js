53b5878e92df93651b973b3b9cb2adf49e48a146
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-04-02 05:17:05 +0000

    디자인 수정 및 스크롤(lazy loading)

--- trunk/ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/model/InstanceList.ejs.js
+++ trunk/ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/model/InstanceList.ejs.js
@@ -1,20 +1,25 @@
 var org_uengine_codi_mw3_model_InstanceList = function(objectId, className) {
 	this.objectId = objectId;
 	this.className = className;
-}
-
-$("#scroll_div").scroll(function(e) {	
-	/*
-	alert($("#scroll_div").scrollTop()+"//"+
-	  	  $('#scroll_div>div').height()+"//"+
-		  $("#scroll_div").height()
-	);
-	*/
-	if($("#scroll_div").scrollTop() + 26 > $('#scroll_div>div').height() - $("#scroll_div").height()){
-		console.log("last");
+	
+	this.divId = "#objDiv_" + objectId;
+	
+	$(this.divId).parent().unbind('scroll');
+	
+	var object = mw3.objects[objectId];
+	
+	if(object.metaworksContext.when == 'more'){
+		$(this.divId).parent().bind('scroll', function(e){
+			console.debug('lazy : ' + objectId);
+			
+			var scroll = $("#objDiv_" + objectId).parent();		
+			
+			if(scroll.scrollTop() + 26 > scroll.children('div').height() - scroll.height()){
+				scroll.unbind('scroll');
+				
+				mw3.call(objectId, 'more');	
+			}
+		});
 	}
-});
 
-
-
-
+}
