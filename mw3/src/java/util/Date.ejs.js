60f4663c0fd044f4f492c22b0160609988f16007
somehow <somehow@uengine.org>
2012-04-06 10:18:52 +0000

    date.ejs 수정

--- mw3/src/java/util/Date.ejs.js
+++ mw3/src/java/util/Date.ejs.js
@@ -7,12 +7,16 @@ var java_util_Date = function(objectId, className){
 	var when = $('#' + this.inputId).attr('when');
 	
 	$('#' + this.inputId).datepicker({
-		dateFormat: 'yy-mm-dd',
+		monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],		// 월 한글로 출력
+		dayNamesMin: ['일','월','화','수','목','금','토'],		// 요일 한글로 출력
+	  	dateFormat: 'yy-mm-dd', //데이터 포멧형식
+	  	showMonthAfterYear: true,
 		showOn: 'button',
 		buttonImageOnly: true,
 		buttonImage :'images/waveStyle/calendar.gif'
 	});
 	
+  	
 	$('#' + this.inputId).datepicker("setDate", mw3.objects[objectId]);
 	
 	if(when != 'edit' && when != 'new')
