ea34eab657da05f9e1eadd394044a2eedf43997b
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-04-12 08:35:03 +0000

    modal window size 수정

--- trunk/mw3/src/org/metaworks/widget/ModalWindow.java
+++ trunk/mw3/src/org/metaworks/widget/ModalWindow.java
@@ -1,6 +1,7 @@
 package org.metaworks.widget;
 
 import org.metaworks.annotation.Face;
+import org.metaworks.annotation.Hidden;
 
 @Face(displayName="ModalWindow")
 public class ModalWindow {
@@ -10,8 +11,32 @@ public class ModalWindow {
 	}
 	
 	public ModalWindow(Object panel) {
-		this.panel = panel;
+		setPanel(panel);
 	}
+	
+	public ModalWindow(Object panel, int width, int height) {
+		setPanel(panel);
+		setWidth(width);
+		setHeight(height);
+	}	
+	
+	int width;	
+		@Hidden
+		public int getWidth() {
+			return width;
+		}
+		public void setWidth(int width) {
+			this.width = width;
+		}
+
+	int height;
+		@Hidden
+		public int getHeight() {
+			return height;
+		}
+		public void setHeight(int height) {
+			this.height = height;
+		}
 	
 	Object panel;
 		public Object getPanel() {
