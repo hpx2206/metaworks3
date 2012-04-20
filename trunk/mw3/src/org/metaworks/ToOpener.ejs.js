d159f46721766cfbe46f32ad891953f6df5032fe
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-04-20 06:40:51 +0000

    target=opener 대신 ToOpener 추가

---
+++ trunk/mw3/src/org/metaworks/ToOpener.ejs.js
@@ -0,0 +1,11 @@
+var org_metaworks_ToOpener = function(objectId, className){
+
+	this.objectId = objectId;
+	this.className = className;
+	
+	var object = mw3.objects[objectId];
+
+	mw3.setObject(mw3.recentOpenerObjectId, object.target);
+
+	mw3.removeObject(this.objectId);
+}
