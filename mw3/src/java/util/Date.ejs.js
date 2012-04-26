a7d2d36371c207930e65ba90f5663d7990678a84
somehow <somehow@uengine.org>
2012-04-26 05:19:32 +0000

    Date.ejs 의 view 모드일때 input 사용 안하게 수정 및 picker 아이콘 제거

--- mw3/src/java/util/Date.ejs.js
+++ mw3/src/java/util/Date.ejs.js
@@ -5,34 +5,29 @@ var java_util_Date = function(objectId, className){
 	this.inputId = mw3.createInputId(objectId);
 	
 	var when = $('#' + this.inputId).attr('when');
-	
-	$('#' + this.inputId).datepicker({
-		monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],		// 월 한글로 출력
-		dayNamesMin: ['일','월','화','수','목','금','토'],		// 요일 한글로 출력
-	  	dateFormat: 'yy-mm-dd', //데이터 포멧형식
-	  	showMonthAfterYear: true,
-		showOn: 'button',
-		buttonImageOnly: true,
-		buttonImage :'images/waveStyle/calendar.gif'
-	});
-	
-  	
-	$('#' + this.inputId).datepicker("setDate", mw3.objects[objectId]);
-	
-	if(when != 'edit' && when != 'new')
-		$('#objDiv_' + objectId + ' .ui-datepicker-trigger').remove();	
+		
+	if(when == 'edit' || when == 'new'){
+		$('#' + this.inputId).datepicker({
+			monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],		// 월 한글로 출력
+			dayNamesMin: ['일','월','화','수','목','금','토'],		// 요일 한글로 출력
+		  	dateFormat: 'yy-mm-dd', //데이터 포멧형식
+		  	showMonthAfterYear: true
+		});	
+		
+		$('#' + this.inputId).datepicker("setDate", mw3.objects[objectId]);
+	}
 }
 
 java_util_Date.prototype.getValue = function(){
 	
 	var inputObj = $("#" + this.inputId);
 	
-	if(inputObj.lenght){	
+	if(inputObj.length){	
 		var selDate = new Date($("#" + this.inputId).datepicker("getDate"));
 	
 		return selDate.getTime();
 	}else{
-		return null;
+		return mw3.objects[this.objectId];
 	}
 		
 }
