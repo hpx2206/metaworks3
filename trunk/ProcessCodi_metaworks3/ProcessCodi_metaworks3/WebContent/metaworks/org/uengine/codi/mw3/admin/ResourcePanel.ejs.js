f1af4b48b25f3fe8b54651324ed71dbd56b4877d
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-05-18 02:16:58 +0000

    이상한 폴더 삭제

--- trunk/ProcessCodi_metaworks3/ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/admin/ResourcePanel.ejs.js
+++
@@ -1,9 +0,0 @@
-var org_uengine_codi_mw3_admin_ResourcePanel = function(objectId, className){
-	this.objectId = objectId;
-	this.className = className;
-	this.divId = "#objDiv_" + objectId;
-	
-	
-	$(this.divId).css('height', '100%');
-	
-}
--- trunk/ProcessCodi_metaworks3/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/ActionNode.java
+++
@@ -1,8 +0,0 @@
-package org.uengine.codi.mw3.model;
-
-public class ActionNode extends DecisionTreeNode {
-
-	public void executeAction() {
-		System.out.println("Execute Action");
-	}
-}
