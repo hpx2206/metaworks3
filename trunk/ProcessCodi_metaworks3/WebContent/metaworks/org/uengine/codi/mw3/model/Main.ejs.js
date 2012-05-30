f49e192ce3553656e65f357dd268030b615e49fb
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-05-30 05:13:00 +0000

    강태욱 과장 요청 작업

--- trunk/ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/model/Main.ejs.js
+++ trunk/ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/model/Main.ejs.js
@@ -2,5 +2,13 @@ var org_uengine_codi_mw3_model_Main = function(objectId, className){
 	this.objectId = objectId;
 	this.className = className;
 	
-	$("#objDiv_" + objectId).css("height", "100%");
+	$('#objDiv_' + objectId).css('height', '100%');
+	
+	$(document).mousedown(function(e){
+		$('.mw3_popup').each(function(){
+			var objectId = $(this).attr('objectId');
+			
+			mw3.getFaceHelper(objectId).checkPosition(e);
+		});
+	});
 }
--- trunk/ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/model/Popup.ejs.js
+++ trunk/ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/model/Popup.ejs.js
@@ -1,8 +1,10 @@
 var org_uengine_codi_mw3_model_Popup = function(objectId, className) {
 	this.objectId = objectId;
 	this.className = className;
+	this.divId = "#objDiv_" + this.objectId;
+	this.divObj = $("#objDiv_" + this.objectId).parent();
 	
-	$("#objDiv_" + this.objectId).parent().addClass("mw3_popup").attr("objectid", objectId).addClass("clue-right-rounded").addClass("cluetip-rounded").css({position:'absolute','z-index':97,display:'none'});
+	this.divObj.addClass("mw3_popup").attr("objectid", objectId).addClass("clue-right-rounded").addClass("cluetip-rounded").css({position:'absolute','z-index':97,display:'none'});
 	
 	
 	//var openerDiv = $("#objDiv_" + mw3.recentOpenerObjectId);
@@ -12,43 +14,54 @@ var org_uengine_codi_mw3_model_Popup = function(objectId, className) {
 	this.createPopup(400, 300, mw3.mouseX, mw3.mouseY);	
 }
 
-org_uengine_codi_mw3_model_Popup.prototype.createPopup = function(w,h,x,y){
-	var popLayerWidth = w;
-	var popLayerHeight = h;
-	
-	var bodyHeight = $('body').height();
-	var bodyWidth = $('body').width();
-	
-	var divId = '#objDiv_' + this.objectId;
-	
-	$(divId).parent().show();
-	$(divId).parent().css({top:y,left:x,width:popLayerWidth,height:popLayerHeight});
-	$(divId).parent().removeClass('clue-left-rounded');
-	$(divId).parent().addClass('clue-right-rounded');
-	$(divId + ' #addcontact-con').height(h-43);
-	$(divId + ' .cluetip-arrows').css({top:10});
-	
-	if(bodyWidth && popLayerWidth + x > bodyWidth){
-		$(divId).parent().css({left:x - popLayerWidth});
-		$(divId).parent().removeClass('clue-right-rounded');
-		$(divId).parent().addClass('clue-left-rounded');
-	};
-	
-	if(bodyHeight && popLayerHeight + y > bodyHeight){    	
-	 	$(divId).parent().css({top:y - popLayerHeight});
-	 	$(divId + ' .cluetip-arrows').css({top:popLayerHeight-40});
-	};
+org_uengine_codi_mw3_model_Popup.prototype = {
+	createPopup : function(w,h,x,y){
+		var popLayerWidth = w;
+		var popLayerHeight = h;
+		
+		var bodyHeight = $('body').height();
+		var bodyWidth = $('body').width();
+		
+		var divId = '#objDiv_' + this.objectId;
+		
+		$(divId).parent().show();
+		$(divId).parent().css({top:y,left:x,width:popLayerWidth,height:popLayerHeight});
+		$(divId).parent().removeClass('clue-left-rounded');
+		$(divId).parent().addClass('clue-right-rounded');
+		$(divId + ' #addcontact-con').height(h-43);
+		$(divId + ' .cluetip-arrows').css({top:10});
+		
+		if(bodyWidth && popLayerWidth + x > bodyWidth){
+			$(divId).parent().css({left:x - popLayerWidth});
+			$(divId).parent().removeClass('clue-right-rounded');
+			$(divId).parent().addClass('clue-left-rounded');
+		};
+		
+		if(bodyHeight && popLayerHeight + y > bodyHeight){    	
+		 	$(divId).parent().css({top:y - popLayerHeight});
+		 	$(divId + ' .cluetip-arrows').css({top:popLayerHeight-40});
+		};
 
-};
+	},
+	destoryPopup : function() {
+		var divId = '#objDiv_' + this.objectId;
+		
+		$(divId).parent().remove();
+	},
+	destroy : function() {
+		this.destoryPopup();
+	},
+	checkPosition : function(e){
+		var l_position = this.divObj.offset();
+		l_position.right = parseInt(l_position.left) + this.divObj.width();
+		l_position.bottom = parseInt(l_position.top) + this.divObj.height();
 
-org_uengine_codi_mw3_model_Popup.prototype.destroyPopup = function() {
-	var divId = '#objDiv_' + this.objectId;
-	
-	$(divId).parent().remove();
-}
 
-org_uengine_codi_mw3_model_Popup.prototype.destroy = function() {
-	var divId = '#objDiv_' + this.objectId;
-	
-	$(divId).parent().remove();
+		if ( ( l_position.left <= e.pageX && e.pageX <= l_position.right )
+				&& ( l_position.top <= e.pageY && e.pageY <= l_position.bottom ) ){
+			
+		}else{
+			this.destoryPopup();
+		}
+	}		
 }
