0808ff340cdfcc4e36ebb301722393a589528006
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-05-30 13:15:51 +0000

    workflowy 추가 작업

---
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/knowledge/WfPanel.java
@@ -0,0 +1,68 @@
+package org.uengine.codi.mw3.knowledge;
+
+import org.metaworks.ContextAware;
+import org.metaworks.MetaworksContext;
+
+public class WfPanel implements ContextAware {
+	
+	WfNode wfNode;	
+		public WfNode getWfNode() {
+			return wfNode;
+		}
+		public void setWfNode(WfNode wfNode) {
+			this.wfNode = wfNode;
+		}
+
+	MetaworksContext metaworksContext;
+		public MetaworksContext getMetaworksContext() {
+			return metaworksContext;
+		}
+		public void setMetaworksContext(MetaworksContext metaworksContext) {
+			this.metaworksContext = metaworksContext;
+		}
+		
+	public void load(String nodeId) throws Exception {
+		load(nodeId, MetaworksContext.WHEN_VIEW);
+	}
+	
+	public void load(String nodeId, String metaworksContext) throws Exception {
+		
+		setMetaworksContext(new MetaworksContext());
+		getMetaworksContext().setWhen(metaworksContext);
+		
+		WfNode node = new WfNode();
+		node.getMetaworksContext().setWhen(metaworksContext);
+		node.getMetaworksContext().setHow("ROOT");
+		node.load(nodeId);
+		
+		
+		
+		//node.setName("");				
+		//
+		//node.getMetaworksContext().setWhere("ROOT");
+		
+		/*
+		if(getMetaworksContext().getWhen().equals(MetaworksContext.WHEN_VIEW)){
+			node.load();
+			
+			if(node.getChildNode().size() == 0){
+				WorkflowyNode newNode = new WorkflowyNode(WorkflowyNode.makeId());
+				newNode.setParentNode(node);
+				
+				node.getChildNode().add(newNode);
+				node.getMetaworksContext().setWhen(MetaworksContext.WHEN_EDIT);
+			}
+		}
+		*/
+		
+		setWfNode(node);
+	}		
+	
+	public void clearFocus(){
+		getWfNode().clearFocus();
+	}
+	
+	public WfNode getNode(String nodeId){
+		return getWfNode().getNode(nodeId);
+	}
+}
