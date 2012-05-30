cd6e4bbddbf9ec30077f3defea57efd7c1c63b79
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-05-30 09:43:15 +0000

    강태욱 과장 요청 작업

--- trunk/ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/model/SearchBox.ejs.js
+++ trunk/ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/model/SearchBox.ejs.js
@@ -1,47 +1,49 @@
-//var timeout;
-
 var org_uengine_codi_mw3_model_SearchBox = function(objectId, className) {
 	this.objectId = objectId;
 	this.className = className;
 	this.timeout;
-	this.keyword = '';
+	
+	var object = mw3.objects[this.objectId];
+	if(object && object.keyword)
+		this.keyword = object.keyword;
+
 	
 	$("#search_" + this.objectId).focus();
 }
 
-org_uengine_codi_mw3_model_SearchBox.prototype.getValue = function() {
-	var object = mw3.objects[this.objectId];
+org_uengine_codi_mw3_model_SearchBox.prototype = {
+	getValue : function() {
+		var object = mw3.objects[this.objectId];
 
-	object.keyword = $("#search_" + this.objectId).val();
+		object.keyword = $("#search_" + this.objectId).val();
 
-	return object;
-}
-
-org_uengine_codi_mw3_model_SearchBox.prototype.keyup = function(element) {
-	var keyword = $("#search_" + this.objectId).val();
+		return object;
+	},
+	keyup : function(element) {
+		var keyword = element.value;
+			
+		if(this.keyword == keyword)
+			return false;
 		
-	if(this.keyword == keyword)
-		return false;
-	
-	this.keyword = keyword;
-	
-	var objectId = this.objectId;
-	
-	if (this.timeout) {
-		clearTimeout(this.timeout);
-	}
-	
-	this.timeout = setTimeout(function() {
-		mw3.call(objectId, 'search');
-	}, 500);
-	
-}
-
-org_uengine_codi_mw3_model_SearchBox.prototype.newInstance = function(){
-	
-	var instanceListPanel = mw3.getAutowiredObject("org.uengine.codi.mw3.model.InstanceListPanel");
-	if(instanceListPanel){
-		instanceListPanel.newInstance();
-	}
-	
+		this.keyword = keyword;
+		
+		var objectId = this.objectId;
+		
+		if (this.timeout) {
+			clearTimeout(this.timeout);
+		}
+		
+		this.timeout = setTimeout(function() {
+			mw3.call(objectId, 'search');
+		}, 500);
+		
+	},
+	newInstance : function(){
+		
+		var instanceListPanel = mw3.getAutowiredObject('org.uengine.codi.mw3.model.InstanceListPanel');
+		if(instanceListPanel){
+			instanceListPanel.newInstance();
+		}
+		
+	}		
 }
