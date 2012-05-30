4f9fff6ee3dbf27367c13d7b84ddb2075a3f9ff1
somehow <somehow@uengine.org>
2012-05-30 04:09:51 +0000

    강태욱 과장 요청 작업

--- ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/admin/ResourcePanel.ejs.js
+++ ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/admin/ResourcePanel.ejs.js
@@ -3,7 +3,41 @@ var org_uengine_codi_mw3_admin_ResourcePanel = function(objectId, className){
 	this.className = className;
 	this.divId = "#objDiv_" + objectId;
 	
+	$(this.divId).addClass('mw3_layout').attr('objectId', objectId);
 	
-	$(this.divId).css('height', '100%');
+	var faceHelper = this;	
+	faceHelper.load();		
 	
 }
+
+org_uengine_codi_mw3_admin_ResourcePanel.prototype = {
+		load : function(){
+			var object = mw3.objects[this.objectId];
+			var options = {
+					togglerLength_open:	0, 
+					spacing_open:		0, 
+					spacing_closed:		0,
+					center__onresize:	'mw3.getFaceHelper('+this.objectId+').resizeChild()'
+			}
+
+			this.layout = $(this.divId).layout(options);
+		},
+		destroy : function(){
+			$(this.divId).layout().destroy();
+		},
+		resize : function(){
+			if(this.layout){
+				this.layout.resizeAll();
+				
+				this.resizeChild();
+			}
+		},
+		resizeChild : function(){
+			$(this.divId).find('.mw3_layout:visible').each(function(index, value){
+				var layoutId = value.getAttribute('objectId');
+				
+				if(layoutId)
+					mw3.getFaceHelper(layoutId).resize();
+			});
+		}
+	}
