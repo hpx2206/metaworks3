df7debf4aa421ef83fd389300496a4cda548c482
somehow <somehow@uengine.org>
2012-05-31 06:31:54 +0000

    window layout 작업

---
+++ mw3/src/genericfaces/Window.java
@@ -0,0 +1,11 @@
+package genericfaces;
+
+public class Window {
+	String title;
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}	
+}
