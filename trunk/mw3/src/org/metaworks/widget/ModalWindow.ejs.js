61364e1fbd10845764c9540efdecf93dffeee1f1
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-05-31 06:44:00 +0000

    window layout 작업

--- trunk/mw3/src/org/metaworks/widget/ModalWindow.ejs.js
+++ trunk/mw3/src/org/metaworks/widget/ModalWindow.ejs.js
@@ -5,34 +5,52 @@ var org_metaworks_widget_ModalWindow = function(objectId, className) {
 
 	var object = mw3.objects[this.objectId];
 
-	if (object.open) {
+	if(object){
 		var title = object.title;
-
+		
 		if (title == null)
 			title = $('#title_' + objectId).val();
-
-		var options = {};
-
-		options['title'] = title;
-		options['modal'] = true;
-		options['close'] = function(event, ui) {
-			if (typeof event.eventPhase != 'undefined') {
-				mw3.getFaceHelper(objectId).close();
+		
+		if(object.metaworksContext && object.metaworksContext.where == 'mobile'){
+			var modal = $(this.divId);			
+			var popup = modal.parent();
+			
+			var header = '<div data-role=\"header\" data-theme=\"d\"><h1>' + title + '</h1></div>';			
+ 			
+			modal.attr('data-role', 'content');
+			modal.attr('data-theme', 'c');			
+			
+			popup.attr('id', 'dialog_' + this.objectId);
+			popup.attr('data-role', 'dialog');
+			popup.prepend(header);
+			
+			$.mobile.changePage('#dialog_' + this.objectId);
+		}else{ 
+			if (object.open) {		
+				var options = {};
+		
+				options['title'] = title;
+				options['modal'] = true;
+				options['close'] = function(event, ui) {
+					if (typeof event.eventPhase != 'undefined') {
+						mw3.getFaceHelper(objectId).close();
+					}
+				}
+		
+				if (object.width)
+					options['width'] = object.width;
+				
+				if (object.height)
+					options['height'] = object.height;
+				else
+					options['height'] = $(window).height() - 100;
+				
+				$("#dialog:ui-dialog").dialog("destroy");
+				$(this.divId).dialog(options);
+			} else {
+				$(this.divId).css('display', 'none');
 			}
 		}
-
-		if (object.width)
-			options['width'] = object.width;
-		
-		if (object.height)
-			options['height'] = object.height;
-		else
-			options['height'] = $(window).height() - 100;
-		
-		$("#dialog:ui-dialog").dialog("destroy");
-		$(this.divId).dialog(options);
-	} else {
-		$(this.divId).css('display', 'none');
 	}
 }
 
