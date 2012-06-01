804952fe5521a5b80bde43f8684076ddce309b1a
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-06-01 04:47:54 +0000

    contact list 분리 작업

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/ContactListPanel.ejs.js
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/ContactListPanel.ejs.js
@@ -1,7 +1,6 @@
 var org_uengine_codi_mw3_model_ContactListPanel = function(objectId, className){
 	this.objectId = objectId;
 	this.className = className;	
-	this.divId = mw3._getObjectDIvId(this.objectId);
 	
 	$("#accordion_" + this.objectId).accordion({ fillSpace:	true });
 }
