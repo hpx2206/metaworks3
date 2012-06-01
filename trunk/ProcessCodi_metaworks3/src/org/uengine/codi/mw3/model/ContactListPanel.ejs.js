bc2fddb8112c242c70dcfaf48ac53764dfb0c92d
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-06-01 05:15:54 +0000

    window layout 작업

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/ContactListPanel.ejs.js
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/ContactListPanel.ejs.js
@@ -2,5 +2,12 @@ var org_uengine_codi_mw3_model_ContactListPanel = function(objectId, className){
 	this.objectId = objectId;
 	this.className = className;	
 	
+	$('#objDiv_' + this.objectId).height('100%').addClass('mw3_resize').attr('objectId', this.objectId);
 	$("#accordion_" + this.objectId).accordion({ fillSpace:	true });
+}
+
+org_uengine_codi_mw3_model_ContactListPanel.prototype = {
+	resize : function(){
+		$("#accordion").accordion("resize");
+	}
 }
