6b814e7f984a63578a9d1fabf6ada9e7ff68ca06
somehow <somehow@uengine.org>
2012-06-04 02:47:51 +0000

    facehelper null 확인

--- ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/model/Followers.ejs.js
+++ ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/model/Followers.ejs.js
@@ -8,11 +8,11 @@ var org_uengine_codi_mw3_model_Followers = function(objectId, className){
 
 org_uengine_codi_mw3_model_Followers.prototype = {
 	startLoading : function(){
-		if(this.windowObjectId)
+		if(this.windowObjectId && mw3.getFaceHelper(this.windowObjectId) && mw3.getFaceHelper(this.windowObjectId).startLoading)
 			mw3.getFaceHelper(this.windowObjectId).startLoading();
 	},
 	endLoading : function(){
-		if(this.windowObjectId)
+		if(this.windowObjectId && mw3.getFaceHelper(this.windowObjectId) && mw3.getFaceHelper(this.windowObjectId).endLoading)
 			mw3.getFaceHelper(this.windowObjectId).endLoading();
 	},
 	showStatus : function(message){
