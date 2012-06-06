a9d2b58d64d0606badbf51280231ae8e35c4a3b1
jyjang@uengine.org <jyjang@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-06-06 18:27:27 +0000

    Guided Tour

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/Login.ejs.js
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/Login.ejs.js
@@ -2,6 +2,8 @@ var org_uengine_codi_mw3_Login = function(objectId, className){
  this.objectId = objectId;
  this.className = className;
  
+ var tryGuidedTour = false;
+ 
  var object = mw3.objects[this.objectId]; 
  
  //mw3.getInputElement(this.objectId, 'userId').value = 'test';
@@ -10,5 +12,12 @@ var org_uengine_codi_mw3_Login = function(objectId, className){
  mw3.getInputElement(this.objectId, 'userId').focus();
  
  //object.login();
+ 
+	if(!tryGuidedTour && object.alias == null){
+		setTimeout("mw3.test(" +objectId + ", 'first', {guidedTour: true})", 1000);
+
+		tryGuidedTour = true;
+	}
+
  
 }
