b7b3331eeec2c1fcbbd0f5a59b6ca46f2cb9a7d0
jyjang <jyjang@uengine.org>
2012-06-06 18:27:27 +0000

    Guided Tour

--- ProcessCodi_metaworks3/src/org/uengine/codi/mw3/ILogin.java
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/ILogin.java
@@ -3,6 +3,7 @@ package org.uengine.codi.mw3;
 import org.metaworks.ServiceMethodContext;
 import org.metaworks.annotation.Id;
 import org.metaworks.annotation.ServiceMethod;
+import org.metaworks.annotation.Test;
 import org.metaworks.dao.IDAO;
 import org.uengine.codi.mw3.common.MainPanel;
 
@@ -24,6 +25,7 @@ public interface ILogin extends IDAO{
 	public String getDefId();
 	public void setDefId(String defId);
 		
+	@Test(scenario="first", starter=true, instruction="관리자 아이디/패스워스 test/test 를 입력하고 로그인을 합니다.", next="autowiredObject.org.uengine.codi.mw3.model.InstanceListPanel.newInstance()")
 	@ServiceMethod(callByContent=true, target=ServiceMethodContext.TARGET_SELF)
 	public Object login() throws Exception;	
 	
