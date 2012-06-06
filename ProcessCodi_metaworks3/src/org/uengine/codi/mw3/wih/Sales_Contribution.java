46c17c6bc0199c0d1463a8b1fba64a7b2df1ce7c
sleepphoenix <sleepphoenix@uengine.org>
2012-06-06 19:09:34 +0000

    영업활동관리 프로세스

---
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/wih/Sales_Contribution.java
@@ -0,0 +1,73 @@
+package org.uengine.codi.mw3.wih;
+
+import java.io.Serializable;
+
+import org.metaworks.MetaworksContext;
+import org.metaworks.annotation.Face;
+import org.metaworks.annotation.Hidden;
+import org.uengine.codi.ITool;
+
+public class Sales_Contribution implements ITool , Serializable {
+	String instanceId;
+	@Hidden
+		public String getInstanceId() {
+			return instanceId;
+		}
+		public void setInstanceId(String instanceId) {
+			this.instanceId = instanceId;
+		}
+		
+    MetaworksContext metaworksContext;
+        public MetaworksContext getMetaworksContext(){
+            return this.metaworksContext;
+        }
+        public void setMetaworksContext(MetaworksContext metaworksContext){
+            this.metaworksContext = metaworksContext;
+        }
+     
+        
+        
+    @Override
+	public void onLoad() {
+        this.metaworksContext = new MetaworksContext();
+        metaworksContext.setWhen(MetaworksContext.WHEN_NEW);
+		// TODO Auto-generated method stub
+        
+	}
+
+	@Override
+	public void beforeComplete() {
+		// TODO Auto-generated method stub
+		//setCompYnVar(compYn == true ? "Y" : "N");
+	}
+
+	@Override
+	public void afterComplete() {
+		// TODO Auto-generated method stub	
+		
+	}
+	
+	String contribution_evaluation;
+	
+	String special_contents;
+	
+	@Face(displayName="기여도 평가")
+	public String getContribution_evaluation() {
+		return contribution_evaluation;
+	}
+	public void setContribution_evaluation(String contribution_evaluation) {
+		this.contribution_evaluation = contribution_evaluation;
+	}
+	
+	@Face(displayName="기타 특이사항")
+	public String getSpecial_contents() {
+		return special_contents;
+	}
+	public void setSpecial_contents(String special_contents) {
+		this.special_contents = special_contents;
+	}
+	
+	
+	
+	
+}
