c6293e36f7caed4a463aa27669368e036fffd5ee
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-06-12 05:56:18 +0000

    date 추상화 추가

---
+++ trunk/ProcessCodi_metaworks3/src/java/util/Date.ejs.js
@@ -0,0 +1,37 @@
+var java_util_Date = function(objectId, className){
+	this.objectId = objectId;
+	this.className = className;
+	
+	this.inputId = mw3.createInputId(objectId);
+	this.divObj = $('#' + this.inputId);
+		
+	var when = this.divObj.attr('when');
+		
+	if(when == 'edit' || when == 'new'){
+		this.divObj.datepicker({
+			monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],		// 월 한글로 출력
+			dayNamesMin: ['일','월','화','수','목','금','토'],		// 요일 한글로 출력
+		  	dateFormat: 'yy-mm-dd', //데이터 포멧형식
+		  	showMonthAfterYear: true
+		});	
+		
+		this.divObj.datepicker("setDate", mw3.objects[this.objectId]);
+	}else{
+		jQuery("abbr.timeago").timeago();
+	}
+}
+
+java_util_Date.prototype = {
+	getValue : function(){
+		var when = this.divObj.attr('when');
+		if(when == 'edit' || when == 'new'){
+			return this.divObj.datepicker("getDate");
+		}else{
+			return mw3.objects[this.objectId];
+		}
+	},
+	clear : function(){
+		mw3.objects[this.objectId] = null;
+		this.divObj.datepicker("setDate", null);
+	}
+}
