cd3c84959f049dbf028e9bec3a8aea0eaa61fce6
sleepphoenix <sleepphoenix@uengine.org>
2012-08-13 06:45:04 +0000

    WebContent 이하의 ejs 파일을 src 폴더로 이동

--- ProcessCodi_metaworks3/WebContent/metaworks/org/uengine/codi/mw3/model/PinterestMain.ejs.js
+++
@@ -1,7 +0,0 @@
-var org_uengine_codi_mw3_model_PinterestMain = function(objectId, className) {
-
-	Set_Cookie("codi.lastVisit", "pinterest", 10, "/", "", "");
-//	$('#pinterest_container').height($(window).height() - 52);
-
-}
-
