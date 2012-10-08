f60c8d934b80169f8fc2bd65f6fa64af71258e8d
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2012-10-08 10:17:08 +0000

    classDefinition 을 context 에 따라 formDefinition 화

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/FormDesignerContentPanel.java
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/FormDesignerContentPanel.java
@@ -1,6 +1,9 @@
 package org.uengine.codi.mw3.model;
 
+import javax.servlet.http.HttpSession;
+
 import org.metaworks.annotation.Face;
+import org.metaworks.dao.TransactionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.uengine.codi.mw3.admin.ClassDefinition;
 import org.uengine.kernel.GlobalContext;
@@ -20,10 +23,17 @@ public class FormDesignerContentPanel extends ContentWindow {
 		}
 	
 	public void newForm(String parentFolder){
+		HttpSession session = TransactionContext.getThreadLocalInstance().getRequest().getSession(); 
+		String userId = (String)session.getAttribute("userId");
+		
+		User user = new User();
+		user.setUserId(userId);
+		
 		formDefinition = new ClassDefinition();
 		formDefinition.getMetaworksContext().setWhere("form");
 		formDefinition.setParentFolder(parentFolder);
 		formDefinition.setPackageName(parentFolder);
+		formDefinition.setAuthor(user);
 		formDefinition.load();
 	}
 	
