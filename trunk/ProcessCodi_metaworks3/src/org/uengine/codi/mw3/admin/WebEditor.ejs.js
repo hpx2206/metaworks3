c242580739dd6ea4294a2e22686b26b697665dc0
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2013-01-09 08:24:43 +0000

    web editor 오류 수정

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/admin/WebEditor.ejs.js
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/admin/WebEditor.ejs.js
@@ -2,22 +2,23 @@ var org_uengine_codi_mw3_admin_WebEditor = function(objectId, className) {
 	this.objectId = objectId;
 	this.className = className;
 	this.divId = mw3._getObjectDivId(this.objectId);
+	this.object = mw3.objects[this.objectId];
 	
 	//$('#' + this.divId).css('height', '640px');
 	
 	this.editorId = 'ckEditor_' + this.objectId;
 	
-	this.editor = CKEDITOR.replace(this.editorId, {
-		fullPage : true,
-		extraPlugins : 'docprops',
-		htmlEncodeOutput : false,
-		entities : false
-	});
-	
-	var webEditor = mw3.objects[objectId];
-	
-	this.editor.setData(webEditor.contents);
-}
+	if($('#' + this.editorId).length > 0){
+		this.editor = CKEDITOR.replace(this.editorId, {
+			fullPage : true,
+			extraPlugins : 'docprops',
+			htmlEncodeOutput : false,
+			entities : false		
+		});
+		
+		this.editor.setData(this.object.contents);		
+	}
+};
 
 org_uengine_codi_mw3_admin_WebEditor.prototype.getValue = function() {
 	
@@ -25,8 +26,9 @@ org_uengine_codi_mw3_admin_WebEditor.prototype.getValue = function() {
 		var editor = {
 			__className : 'org.uengine.codi.mw3.admin.WebEditor',
 			contents : this.editor.getData()
-		}
+		};
+		
 		return editor;
 	}
 	
-}
+};
