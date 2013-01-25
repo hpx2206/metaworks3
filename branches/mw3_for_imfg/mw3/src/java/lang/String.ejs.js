61198b9056870fff2200328e80f5df926bd8d25f
sleepphoenix@uengine.org <sleepphoenix@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2013-01-25 10:15:45 +0000

    아직.. 작업중이어서.. 취소...

--- branches/mw3_for_imfg/mw3/src/java/lang/String.ejs.js
+++
@@ -1,4 +0,0 @@
-var java_lang_String = function(objectId, className){
-	this.objectId = objectId;
-	this.className = className;
-}
--- branches/mw3_for_imfg/mw3/src/org/metaworks/website/Main.ejs.js
+++
@@ -1,7 +0,0 @@
-org_metaworks_website_Main = function(objectId, className){
-	this.objectId = objectId;
-	this.className = className;
-
-	outerLayout = $('#container').layout(); 
-
-}
