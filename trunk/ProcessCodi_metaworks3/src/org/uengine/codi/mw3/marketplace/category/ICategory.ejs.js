1ceef771bb05e540f644943538fd490d41f403ed
jisun@uengine.org <jisun@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2013-03-20 06:49:12 +0000

    ejs.js 경로수정

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/marketplace/category/ICategory.ejs.js
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/marketplace/category/ICategory.ejs.js
@@ -1,18 +1,18 @@
-var org_uengine_processmarket_ICategory = function(objectId, className){
+var org_uengine_codi_mw3_marketplace_category_ICategory = function(objectId, className){
 	this.objectId = objectId;
 	this.className = className;
-}
+};
 
 org_uengine_processmarket_ICategory.prototype.showStatus = function(status){
-}
+};
 
 org_uengine_processmarket_ICategory.prototype.startLoading = function(){
 	
-}
+};
 
 org_uengine_processmarket_ICategory.prototype.endLoading = function(){
 
-}
+};
 
 org_uengine_processmarket_ICategory.prototype.showChildrenCategories = function(){
 	var currCategory = mw3.getObject(this.objectId);
@@ -31,5 +31,4 @@ org_uengine_processmarket_ICategory.prototype.showChildrenCategories = function(
 			});
 		}
 	}
-}
-
+};
