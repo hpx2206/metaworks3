85306b206e1a51ea5359fe86eba86e784c14f480
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2013-03-21 01:19:10 +0000

    cloud ide

---
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/ide/editor/java/JavaCodeEditor.java
@@ -0,0 +1,15 @@
+package org.uengine.codi.mw3.ide.editor.java;
+
+import org.uengine.codi.mw3.ide.editor.Editor;
+
+public class JavaCodeEditor extends Editor {
+
+	public JavaCodeEditor(){
+		super();
+	}
+	
+	public JavaCodeEditor(String filename) {
+		super(filename);
+	}
+
+}
