865f934419ac49cbf8126ed40ba66d89fc645724
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2013-04-04 13:38:43 +0000

    SelectBox 의 수

--- trunk/mw3/src/org/metaworks/component/SelectBox.ejs.js
+++ trunk/mw3/src/org/metaworks/component/SelectBox.ejs.js
@@ -4,6 +4,7 @@ var org_metaworks_component_SelectBox = function(objectId, className) {
 	
 	var object = mw3.objects[this.objectId];
 	
+	/*
 	if(object != null && object.__descriptor && object.__descriptor.getOptionValue('changeEvent')){
 		$('#' + mw3.createInputId(this.objectId)).bind('change', {objectId : this.objectId},function(event){
 			var change = $(this).find('option:selected');
@@ -11,12 +12,14 @@ var org_metaworks_component_SelectBox = function(objectId, className) {
 			mw3.getFaceHelper(event.data.objectId).change(change.val(), change.text());
 		});
 	}
+	*/
 };
 
 org_metaworks_component_SelectBox.prototype = {
 	getValue : function(){
 		var object = mw3.objects[this.objectId];
-		var combo = $('#' + mw3.createInputId(this.objectId));
+		
+		var combo = $('#select_' + this.objectId);
 		
 		if(combo.length > 0){
 			var change = combo.find('option:selected');
