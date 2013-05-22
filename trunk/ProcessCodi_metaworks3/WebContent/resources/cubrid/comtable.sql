bd811c20ff5f3e0fdce1eeabc2f2aac7d3461fa0
jisun@uengine.org <jisun@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2013-05-22 04:40:10 +0000

    cubrid sql 스키마

--- trunk/ProcessCodi_metaworks3/WebContent/resources/cubrid/comtable.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO "comtable" ("comcode", "comname", "description", "isdeleted", "repmail", "repmlhst", "repmlpwd") VALUES ('uengine.org', 'uengine.org', NULL, NULL, NULL, NULL, NULL);
-INSERT INTO "comtable" ("comcode", "comname", "description", "isdeleted", "repmail", "repmlhst", "repmlpwd") VALUES ('CloudManager', 'CloudManager', '', '', '', '', '');
-INSERT INTO "comtable" ("comcode", "comname", "description", "isdeleted", "repmail", "repmlhst", "repmlpwd") VALUES ('uenginecloud.com', 'uenginecloud.com', '', '', '', '', '');
