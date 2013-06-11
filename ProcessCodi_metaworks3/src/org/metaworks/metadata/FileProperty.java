8cd8d0d94809db63412d05f2bf6297941323442e
jisun <jisun@uengine.org>
2013-06-11 07:43:58 +0000

    파일프로퍼티 변수 삭제

--- ProcessCodi_metaworks3/src/org/metaworks/metadata/FileProperty.java
+++ ProcessCodi_metaworks3/src/org/metaworks/metadata/FileProperty.java
@@ -1,6 +1,5 @@
 package org.metaworks.metadata;
 
-import org.metaworks.ContextAware;
 import org.metaworks.MetaworksContext;
 import org.metaworks.annotation.Available;
 import org.metaworks.annotation.Hidden;
@@ -10,19 +9,11 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 
 @XStreamAlias("MetadataProperty")
-public class FileProperty extends MetadataProperty implements ContextAware{
+public class FileProperty extends MetadataProperty{
 	
 	public FileProperty() {
 		setType(MetadataProperty.FILE_PROP);
 	}
-	
-	MetaworksContext metaworksContext;
-		public MetaworksContext getMetaworksContext() {
-			return metaworksContext;
-		}
-		public void setMetaworksContext(MetaworksContext metaworksContext) {
-			this.metaworksContext = metaworksContext;
-		}
 
 	String value;
 		@Hidden
