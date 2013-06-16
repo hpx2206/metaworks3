910cce0a363c00189e3ea380c76a32b0bf1aa61c
jisun <jisun@uengine.org>
2013-06-16 07:47:49 +0000

    소비자셀프서비스 포탈

--- ProcessCodi_metaworks3/src/org/uengine/codi/mw3/selfservice/SelfServiceControlPanel.ejs.js
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/selfservice/SelfServiceControlPanel.ejs.js
@@ -17,10 +17,24 @@ var org_uengine_codi_mw3_selfservice_SelfServiceControlPanel = function(objectId
 	
 	accordionDiv.accordion({
 		fillSpace:	true ,
-		active: 0,
+		active: 3,
 		collapsible: true
 		});
 	
+	var accordionListDiv = accordionDiv.children("#sp_accordion_list_" + this.objectId);
+	
+//	accordionDiv.click(function(){
+//		
+//		var object = mw3.objects[objectId];
+//		console.log("accordionDiv");
+//		console.log(object);
+//	});
+	
+	accordionListDiv.click(function(){
+		console.log("accordionListDiv");
+		console.log(this.object);
+	});
+
 };
 
 org_uengine_codi_mw3_selfservice_SelfServiceControlPanel.prototype = {
