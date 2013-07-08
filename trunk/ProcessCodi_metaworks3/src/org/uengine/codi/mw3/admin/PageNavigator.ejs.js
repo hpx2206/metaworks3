f6346bb7b8d437ed2140f438e975f15ba583ca90
jisun@uengine.org <jisun@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2013-07-08 08:27:28 +0000

    페이지 이동 플립

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/admin/PageNavigator.ejs.js
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/admin/PageNavigator.ejs.js
@@ -1,29 +1,38 @@
-var org_uengine_codi_mw3_admin_PageNavigator = function(objectId, className){
+var org_uengine_codi_mw3_admin_PageNavigator= function(objectId, className){
 	
 	this.objectId = objectId;
 	this.className = className;
+	this.objectDivId = mw3._getObjectDivId(this.objectId);
+	this.objectDiv = $('#' + this.objectDivId);
+	
+	this.object = mw3.objects[this.objectId];
 
+	if(this.object == null)
+		return true;	
+	
 	$('#objDiv_' + objectId).addClass('pageflip');	
 	
 	//Page Flip on hover
-	$(".pageflip").hover(function() {
-		$(".pageflip img , .msg_block").stop()
-			.animate({
-				width: '307px', 
-				height: '319px'
-			}, 700); 
-		} , function() {
-		$(".pageflip img").stop() 
-			.animate({
-				width: '42px', 
-				height: '42px'
-			}, 700);
-		$(".msg_block").stop() 
-			.animate({
-				width: '42px', 
-				height: '42px'
-			}, 700);
-	});	
+	$(".pageflip").hover(
+		function() {
+			$(".pageflip img , .msg_block").stop()
+				.animate({
+					width: '307px', 
+					height: '319px'
+				}, 700); 
+		}, 
+		function() {
+			$(".pageflip img").stop() 
+				.animate({
+					width: '42px', 
+					height: '42px'
+				}, 700);
+			$(".msg_block").stop() 
+				.animate({
+					width: '42px', 
+					height: '42px'
+				}, 700);
+		});	
 	/*
 	$('.goProcess').hover(
 			function(){$('.msg_block').css("background","url(images/waveStyle/pageNavigation_Process.png)");},
