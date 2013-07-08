34f0b2bc046b4178567583c0416e46232f142430
jisun@uengine.org <jisun@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2013-07-08 11:40:36 +0000

    달력 수정

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/calendar/ScheduleCalendar.ejs.js
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/calendar/ScheduleCalendar.ejs.js
@@ -14,7 +14,7 @@ var org_uengine_codi_mw3_calendar_ScheduleCalendar = function(objectId, classNam
 			faceHelper.load();
 		}
 	}
-}
+};
 
 org_uengine_codi_mw3_calendar_ScheduleCalendar.prototype = {
 	load : function(){
@@ -45,7 +45,6 @@ org_uengine_codi_mw3_calendar_ScheduleCalendar.prototype = {
 		}else{
 			calendar.fullCalendar({
 				height: 650,
-	
 				header: {
 					left: 'prevYear,prev,next,nextYear today' ,
 					center: 'title',
@@ -54,9 +53,19 @@ org_uengine_codi_mw3_calendar_ScheduleCalendar.prototype = {
 				},
 				selectable: true,
 				selectHelper: true,
+				editable: true,
 				events: object.data,
-				dayClick: function(date, allDay, jsEvent, view) {
-					mw3.getFaceHelper(objectId).dayClick(date, view.name);
+//				dayClick: function(date, allDay, jsEvent, view) {
+//					mw3.getFaceHelper(objectId).dayClick(date, view.name);
+//				},
+				select: function(start, end, allDay) {
+					
+					var calendar = mw3.getObject(objectId);
+					calendar.selDate = start;
+					calendar.endDate = end;
+					calendar.allDay = allDay;
+										
+					calendar.linkScheduleDay();
 				},
 				eventClick: function(calEvent, jsEvent, view) {	
 					mw3.getFaceHelper(objectId).eventClick(calEvent.id, calEvent.callType, view.name);
@@ -85,9 +94,9 @@ org_uengine_codi_mw3_calendar_ScheduleCalendar.prototype = {
 				
 			});		
 		}
-		calendar.fullCalendar('gotoDate', object.selDate);		
+		calendar.fullCalendar('gotoDate', object.selDate);
 	}
-}
+};
 
 org_uengine_codi_mw3_calendar_ScheduleCalendar.prototype.dayClick = function(selDate, viewMode){
 	var object = mw3.getObject(this.objectId);
@@ -96,7 +105,7 @@ org_uengine_codi_mw3_calendar_ScheduleCalendar.prototype.dayClick = function(sel
 	object.viewMode = viewMode;
 
 	object.linkScheduleDay();
-}
+};
 
 org_uengine_codi_mw3_calendar_ScheduleCalendar.prototype.eventClick = function(schdId, callType, viewMode){
 	var object = mw3.getObject(this.objectId);
@@ -105,22 +114,23 @@ org_uengine_codi_mw3_calendar_ScheduleCalendar.prototype.eventClick = function(s
 	object.viewMode = viewMode;
 
 	object.linkScheduleEvent();
-}
+};
 
 org_uengine_codi_mw3_calendar_ScheduleCalendar.prototype.addMyschedule = function(title, instId, dueDate){
 	var calendar = $('#scheduleCalendar_' + this.objectId);
+	var object = mw3.objects[this.objectId];
 	var myEvent = {
 			  title: title,
 			  id: instId,
-			  allDay: true,
+			  allDay: object.allDay,
 			  callType: 'instance',
 			  color: '#0000cd',
-			  start: dueDate,
+			  start: object.selDate,
 			  end: dueDate
 			};
 	calendar.fullCalendar( 'renderEvent', myEvent );
-}
+};
 org_uengine_codi_mw3_calendar_ScheduleCalendar.prototype.resize = function(){
 	var calendar = $('#scheduleCalendar_' + this.objectId);
 	calendar.fullCalendar('render');	
-}
+};
