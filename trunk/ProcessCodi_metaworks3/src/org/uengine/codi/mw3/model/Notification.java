d1cac295d7ac0971c13f37124d450f8f6fbb9cdd
jisun@uengine.org <jisun@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2013-08-07 10:49:55 +0000

    이메일전송 여부 체크여하에 의해 메일전송(기존 메일전송 방법 그대로 사용, 메일 내용 변경 없음)

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/Notification.java
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/Notification.java
@@ -1,40 +1,40 @@
 package org.uengine.codi.mw3.model;
 
+import java.net.URL;
+import java.rmi.RemoteException;
 import java.util.Date;
+import java.util.List;
 
 import org.metaworks.Refresh;
 import org.metaworks.annotation.AutowiredFromClient;
 import org.metaworks.dao.Database;
+import org.metaworks.dao.TransactionContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.uengine.kernel.UEngineException;
+import org.uengine.webservices.emailserver.impl.EMailServerSoapBindingImpl;
+
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
 
 public class Notification extends Database<INotification> implements INotification{
 
 	Long notiId;
-		
-		
 		public Long getNotiId() {
 			return notiId;
 		}
-	
 		public void setNotiId(Long notiId) {
 			this.notiId = notiId;
 		}
 		
-	
 	Date inputDate;
-
-	
 		public Date getInputDate() {
 			return inputDate;
 		}
-	
 		public void setInputDate(Date inputDate) {
 			this.inputDate = inputDate;
 		}
 
-
 	String userId;
-	
 		public String getUserId() {
 			return userId;
 		}
@@ -43,54 +43,42 @@ public class Notification extends Database<INotification> implements INotificati
 			this.userId = userId;
 		}
 
-		
 	String actorId;
-
 		public String getActorId() {
 			return actorId;
 		}
-	
 		public void setActorId(String actorId) {
 			this.actorId = actorId;
 		}
 		
 	IUser actor;
-			
 		public IUser getActor() {
 			return actor;
 		}
-	
 		public void setActor(IUser actor) {
 			this.actor = actor;
 		}
 
-
 	Long instId;
-
 		public Long getInstId() {
 			return instId;
 		}
-	
 		public void setInstId(Long instId) {
 			this.instId = instId;
 		}
 		
 	Long taskId;
-
 		public Long getTaskId() {
 			return taskId;
 		}
-	
 		public void setTaskId(Long taskId) {
 			this.taskId = taskId;
 		}
 		
 	String actAbstract;
-
 		public String getActAbstract() {
 			return actAbstract;
 		}
-	
 		public void setActAbstract(String actAbstract) {
 			
 			if(actAbstract.length() > 97){
@@ -104,11 +92,12 @@ public class Notification extends Database<INotification> implements INotificati
 		public boolean isConfirm() {
 			return confirm;
 		}
-	
 		public void setConfirm(boolean confirm) {
 			this.confirm = confirm;
 		}
 
+	public String url;
+	
 	public void add(final IInstance instance) throws Exception{
 		createDatabaseMe();
 		flushDatabaseMe();
@@ -124,71 +113,80 @@ public class Notification extends Database<INotification> implements INotificati
 			userInfoTemp.setFacebookId(userInfo.getEmpCode());
 		}
 		
-		final IEmployee userInfoDB = userInfoTemp;
-		
-		final Employee actorUserInfo = new Employee();
-		actorUserInfo.setEmpCode(getActorId());
-		
-//TODO: e-mail nofitication		
-//		try{
-//			final IEmployee actorUserInfoDB = actorUserInfo.databaseMe();
-//			
-//			String requestedURL = TransactionContext.getThreadLocalInstance().getRequest().getRequestURL().toString(); 
-//	        String base = requestedURL.substring( 0, requestedURL.lastIndexOf( "/" ) );
-//	        
-//	        URL urlURL = new java.net.URL(base);
-//	       	String host = urlURL.getHost();
-//	       	int port = urlURL.getPort();
-//	       	String protocol = urlURL.getProtocol();
-//	
-//			final String url = protocol + "://" + host + (port == 80 ? "" : ":"+port) + TransactionContext.getThreadLocalInstance().getRequest().getContextPath();
-//		}catch(Exception e){
-//			e.printStackTrace();
-//		}
-//
-//		new Thread(){
-//
-//			@Override
-//			public void run() {
-//				try {
-//					
-//					if(userInfoDB.getEmpCode()==null && userInfoDB.getFacebookId()!=null){
-//						//TODO: this case should be chosen by more deterministic option (e.g. User.network=='facebook') 
-//						FacebookClient facebookClient = new DefaultFacebookClient();
-//						String facebookId = getUserId();
-//						String query = "SELECT uid, name, email, locale FROM user WHERE uid=" + facebookId;
-//						
-//						List<com.restfb.types.User> users = facebookClient.executeQuery(query, com.restfb.types.User.class);
-//						if (users.size() == 0) {
-//							throw new UEngineException("There's no such user [" + facebookId + "]");
-//						} else {
-//							com.restfb.types.User user = users.get(0);
-//							userInfoDB.setEmail(user.getEmail());
-//						}
-//					}
-//					
-//					(new EMailServerSoapBindingImpl()).sendMail(
-//							actorUserInfoDB.getEmail(), 
-//							actorUserInfoDB.getEmpName(),
-//							userInfoDB.getEmail(), 
-//							"[ProcessCodi] " + instance.getName(), 
-//							getActAbstract() + "<p><a href='" + url + "'>Connect to Process Codi for details.</a>", 
-//							null, 
-//							null,
-//							"UTF-8"
-//					);
-//				} catch (RemoteException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				} catch (Exception e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-//			}
-//			
-//			
-//		}.start();
 
+		/**
+		 * TODO: e-mail nofitication
+		 * 
+		 * 성능 이슈가 야기됨.
+		 * follower마다 Emptable에서 데이터를 읽어 오기 때문에 ...(actorUserInfo.databaseMe()) 차후 수정
+		 * 
+		 */
+		if(userInfo.databaseMe().isNotiEmail()){
+			final IEmployee userInfoDB = userInfoTemp;
+			
+			final Employee actorUserInfo = new Employee();
+			actorUserInfo.setEmpCode(getActorId());
+			
+			final IEmployee actorUserInfoDB = actorUserInfo.databaseMe();
+			
+			try{
+				
+				String requestedURL = TransactionContext.getThreadLocalInstance().getRequest().getRequestURL().toString(); 
+		        String base = requestedURL.substring( 0, requestedURL.lastIndexOf( "/" ) );
+		        
+		        URL urlURL = new java.net.URL(base);
+		       	String host = urlURL.getHost();
+		       	int port = urlURL.getPort();
+		       	String protocol = urlURL.getProtocol();
+		
+		       	url = protocol + "://" + host + (port == 80 ? "" : ":"+port) + TransactionContext.getThreadLocalInstance().getRequest().getContextPath();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+
+			new Thread(){
+
+				@Override
+				public void run() {
+					try {
+						
+						if(userInfoDB.getEmpCode()==null && userInfoDB.getFacebookId()!=null){
+							//TODO: this case should be chosen by more deterministic option (e.g. User.network=='facebook') 
+							FacebookClient facebookClient = new DefaultFacebookClient();
+							String facebookId = getUserId();
+							String query = "SELECT uid, name, email, locale FROM user WHERE uid=" + facebookId;
+							
+							List<com.restfb.types.User> users = facebookClient.executeQuery(query, com.restfb.types.User.class);
+							if (users.size() == 0) {
+								throw new UEngineException("There's no such user [" + facebookId + "]");
+							} else {
+								com.restfb.types.User user = users.get(0);
+								userInfoDB.setEmail(user.getEmail());
+							}
+						}
+						
+						(new EMailServerSoapBindingImpl()).sendMail(
+								actorUserInfoDB.getEmail(), 
+								actorUserInfoDB.getEmpName(),
+								userInfoDB.getEmail(), 
+								"[ProcessCodi] " + instance.getName(), 
+								getActAbstract() + "<p><a href='" + url + "'>Connect to Process Codi for details.</a>", 
+								null, 
+								null,
+								"UTF-8"
+						);
+					} catch (RemoteException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+				
+			}.start();
+		}
 	}
 		
 		
