fcc27295477271168fc41755472d1eafca6f1948
sanaloveyou@uengine.org <sanaloveyou@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2013-10-04 10:00:55 +0000

    JMS branch Merge

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/ide/templete/NewProcess.java
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/ide/templete/NewProcess.java
@@ -3,6 +3,7 @@ package org.uengine.codi.mw3.ide.templete;
 import java.io.File;
 
 import org.metaworks.MetaworksException;
+import org.metaworks.Refresh;
 import org.metaworks.Remover;
 import org.metaworks.ServiceMethodContext;
 import org.metaworks.ToAppend;
@@ -11,17 +12,27 @@ import org.metaworks.annotation.Face;
 import org.metaworks.annotation.ServiceMethod;
 import org.metaworks.component.TreeNode;
 import org.metaworks.widget.ModalWindow;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.uengine.codi.mw3.ide.CloudInstanceWindow;
 import org.uengine.codi.mw3.ide.CloudWindow;
 import org.uengine.codi.mw3.ide.ResourceNode;
 import org.uengine.codi.mw3.ide.Templete;
 import org.uengine.codi.mw3.ide.editor.process.ProcessEditor;
+import org.uengine.codi.mw3.model.IInstance;
+import org.uengine.codi.mw3.model.Instance;
+import org.uengine.codi.mw3.model.InstanceViewThreadPanel;
 import org.uengine.codi.mw3.model.Session;
+import org.uengine.codi.mw3.model.WorkItem;
+import org.uengine.processmanager.ProcessManagerRemote;
 
 @Face(displayName="templete.process", ejsPath="dwr/metaworks/genericfaces/FormFace.ejs")
 public class NewProcess extends Templete {
 
 	@AutowiredFromClient
 	public Session session;
+	
+	@Autowired
+	public ProcessManagerRemote processManager;
 	
 	String name;
 		@Face(displayName="$templete.process.name")
@@ -39,14 +50,30 @@ public class NewProcess extends Templete {
 			ResourceNode targetNode = (ResourceNode)clipboard;
 			
 			ResourceNode node = new ResourceNode();
-			node.setName(this.getName() + ".wpd");
+			node.setName(this.getName() + ".process");
 			node.setId(targetNode.getId() + File.separatorChar + node.getName());
 			node.setPath(targetNode.getPath() + File.separatorChar + node.getName());
 			
+			WorkItem workitem = new WorkItem();
+			workitem.session = session;
+			workitem.processManager = processManager;
+			workitem.setType("comment");
+			workitem.setTitle("process thread : "+this.getName());
+			
+			IInstance instance = workitem.save();
+			InstanceViewThreadPanel instanceViewThreadPanel = new InstanceViewThreadPanel();
+			instanceViewThreadPanel.session = session;
+			instanceViewThreadPanel.setInstanceId(instance.getInstId()+"");
+			instanceViewThreadPanel.load();
+			CloudInstanceWindow cloudInstanceWindow = new CloudInstanceWindow();
+			cloudInstanceWindow.setPanel(instanceViewThreadPanel);
+			
 			ProcessEditor editor = new ProcessEditor(node);
+			editor.setProcessDesignerInstanceId(instance.getInstId()+"");
+			editor.getProcessDesigner().getProcessNameView().setAlias(this.getName());
 			editor.save();
 			
-			return new Object[]{new ToAppend(targetNode, node), new ToAppend(new CloudWindow("editor"), editor), new Remover(new ModalWindow())};
+			return new Object[]{new ToAppend(targetNode, node), new ToAppend(new CloudWindow("editor"), editor), new Remover(new ModalWindow()) , new Refresh(cloudInstanceWindow, true)};
 		}else{
 			throw new MetaworksException("finish error");
 		}
