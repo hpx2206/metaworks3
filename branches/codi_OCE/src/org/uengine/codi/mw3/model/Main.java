0fb1ca4dff8c63dab82a6f074563752edfd3a84b
jisun@uengine.org <jisun@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2013-11-11 08:39:53 +0000

    8080 서버반영

--- branches/codi_OCE/src/org/uengine/codi/mw3/model/Main.java
+++ branches/codi_OCE/src/org/uengine/codi/mw3/model/Main.java
@@ -204,20 +204,18 @@ public class Main {
 			
 			setLayout(outerLayout);
 			
-//			if( session.getEmployee().getIsAdmin() ){	// 관리자일 경우만 page flip 이 보임
-			
-			if(!session.getEmployee().isGuest()){
-				//향후 변경해야 할것 으로 보입니다.
-				if("oce".equals(session.getUx()))
-					setPageNavigator(new OcePageNavigator());
-				else if("sns".equals(session.getUx()))
-					setPageNavigator(new OcePageNavigator());
-				else
-					setPageNavigator(new PageNavigator("process"));
+			if( session.getEmployee().getIsAdmin() ){	// 관리자일 경우만 page flip 이 보임
+
+				if(!session.getEmployee().isGuest()){
+					//향후 변경해야 할것 으로 보입니다.
+					if("oce".equals(session.getUx()))
+						setPageNavigator(new OcePageNavigator());
+					else if("sns".equals(session.getUx()))
+						setPageNavigator(new OcePageNavigator());
+					else
+						setPageNavigator(new PageNavigator("process"));
+				}
 			}
-			
-			
-//			}
 		}
 
 	}
