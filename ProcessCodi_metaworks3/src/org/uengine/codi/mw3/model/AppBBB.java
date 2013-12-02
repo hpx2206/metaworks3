c4c076545dc95cf2c48ad05de78a73d3d96dad7e
somehow <somehow@uengine.org>
2013-12-02 04:25:57 +0000

    add app

---
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/AppBBB.java
@@ -0,0 +1,8 @@
+package org.uengine.codi.mw3.model;
+
+import org.metaworks.annotation.Face;
+
+@Face(ejsPath="dwr/metaworks/genericfaces/CleanObjectFace.ejs")
+public class AppBBB {
+
+}
---
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/AppCalendar.java
@@ -0,0 +1,47 @@
+package org.uengine.codi.mw3.model;
+
+import org.metaworks.annotation.Face;
+import org.metaworks.widget.layout.Layout;
+
+@Face(ejsPath="dwr/metaworks/genericfaces/CleanObjectFace.ejs")
+public class AppCalendar {
+
+	Layout content;
+	public Layout getContent() {
+		return content;
+	}
+	public void setContent(Layout content) {
+		this.content = content;
+	}
+
+	public AppCalendar(Session session){
+		Layout layout = new Layout();
+
+		InstanceListPanel instanceListPanel = new InstanceListPanel(session);
+		instanceListPanel.session = session;
+		
+		try{
+			instanceListPanel.switchToScheduleCalendar();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		ContentWindow contentWindow = new ContentWindow();
+		contentWindow.setTitle("$New");
+		
+		NewInstancePanel instancePanel = new NewInstancePanel();
+		instancePanel.session = session;
+		
+		try{
+			instancePanel.load(session);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		layout.setWest(instanceListPanel);
+		layout.setOptions("togglerLength_open:0, spacing_open:0, spacing_closed:0, south__spacing_open:5, west__spacing_open:5, west__size:'40%'");
+		layout.setCenter(instancePanel);
+		
+		this.setContent(layout);
+	}
+}
