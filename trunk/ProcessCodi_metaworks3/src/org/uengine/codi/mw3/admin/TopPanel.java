bb21408f2bbe5b85ce2d038d52f73d7c3209d248
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2013-12-03 08:54:22 +0000

    TopPanel 통합 작

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/admin/TopPanel.java
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/admin/TopPanel.java
@@ -1,25 +1,107 @@
 package org.uengine.codi.mw3.admin;
 
-import org.metaworks.annotation.AutowiredToClient;
-import org.uengine.codi.mw3.menu.MainMenuTop;
+import org.metaworks.MetaworksContext;
+import org.metaworks.annotation.Face;
+import org.uengine.codi.mw3.model.IUser;
+import org.uengine.codi.mw3.model.InstanceSearchBox;
+import org.uengine.codi.mw3.model.NotificationBadge;
+import org.uengine.codi.mw3.model.SearchBox;
 import org.uengine.codi.mw3.model.Session;
+import org.uengine.codi.mw3.model.TodoBadge;
+import org.uengine.codi.mw3.model.User;
 
 public class TopPanel {
 
+	public TopPanel(){
+		
+	}
+	
 	public TopPanel(Session session) {
 		setSession(session);
 		setWindowPanel(new WindowPanel());
-		setMainMenu(new MainMenuTop());		
+		
+		MetaworksContext metaworksContext = new MetaworksContext();
+		metaworksContext.setWhere("topPanel");
+		
+		notificationBadge = new NotificationBadge();
+		
+		todoBadge = new TodoBadge();
+		
+		IUser loginUser = new User();
+		loginUser.setUserId(session.getUser().getUserId());
+		loginUser.setName(session.getUser().getName());
+		loginUser.setMetaworksContext(metaworksContext);
+
+		setLoginUser(loginUser);
+		
+		/*
+		if("oce".equals(session.getUx())){
+			InstanceSearchBox searchBox = new InstanceSearchBox();
+			searchBox.setMetaworksContext(metaworksContext);
+			searchBox.setKeyUpSearch(true);
+			searchBox.setKeyEntetSearch(true);
+			setSearchBox(searchBox);
+		}
+		*/
 	}
 	
-	MainMenuTop mainMenu;
-		public MainMenuTop getMainMenu() {
-			return mainMenu;
+	NotificationBadge notificationBadge;
+		public NotificationBadge getNotificationBadge() {
+			return notificationBadge;
 		}
-		public void setMainMenu(MainMenuTop mainMenu) {
-			this.mainMenu = mainMenu;
+		public void setNotificationBadge(NotificationBadge notificationBadge) {
+			this.notificationBadge = notificationBadge;
 		}
 		
+	TodoBadge todoBadge;
+		public TodoBadge getTodoBadge(){
+			return todoBadge;
+		}
+		public void setTodoBadge(TodoBadge todoBadge){
+			this.todoBadge = todoBadge;
+		}
+		
+	String pageType;
+		public String getPageType() {
+			return pageType;
+		}
+		public void setPageType(String pageType) {
+			this.pageType = pageType;
+		}
+	
+	Session session;
+		public Session getSession() {
+			return session;
+		}
+		public void setSession(Session session) {
+			this.session = session;
+		}
+		
+	IUser loginUser;
+		public IUser getLoginUser() {
+			return loginUser;
+		}
+		public void setLoginUser(IUser loginUser) {
+			this.loginUser = loginUser;
+		}
+		
+	SearchBox searchBox;
+		@Face(options={"keyupSearch"}, values={"true"})
+		public SearchBox getSearchBox() {
+			return searchBox;
+		}
+		public void setSearchBox(SearchBox searchBox) {
+			this.searchBox = searchBox;
+		}
+	
+	String mode;
+		public String getMode() {
+			return mode;
+		}
+		public void setMode(String mode) {
+			this.mode = mode;
+		}
+
 	WindowPanel windowPanel;
 		public WindowPanel getWindowPanel() {
 			return windowPanel;
@@ -27,13 +109,4 @@ public class TopPanel {
 		public void setWindowPanel(WindowPanel windowPanel) {
 			this.windowPanel = windowPanel;
 		}
-
-	Session session;
-		@AutowiredToClient
-		public Session getSession() {
-			return session;
-		}
-		public void setSession(Session session) {
-			this.session = session;
-		}		
 }
