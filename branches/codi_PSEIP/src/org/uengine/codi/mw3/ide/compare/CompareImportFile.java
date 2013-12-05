14fa8d858d53ebe36b3f8b69e28882862d31f354
rucius@uengine.org <rucius@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2013-12-05 07:18:26 +0000

    공공서비스

--- branches/codi_PSEIP/src/org/uengine/codi/mw3/ide/compare/CompareImportFile.java
+++
@@ -1,9 +0,0 @@
-package org.uengine.codi.mw3.ide.compare;
-
-public class CompareImportFile extends CompareFile{
-
-	public CompareImportFile(){
-		setViewType("definitionDiffView");
-		setEditorId(CompareImportFilePanel.FILE_LOCATION);
-	}
-}
