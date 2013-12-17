fc2c5fc137747e0af84dd0259516e471281ac4c5
cjsdus87@uengine.org <cjsdus87@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2013-12-17 08:05:19 +0000

    친구 초대시 친구 목록을 사진으로 바꿈

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/Dept.java
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/Dept.java
@@ -295,7 +295,8 @@ public class Dept extends Database<IDept> implements IDept {
 
 	@Override
 	public void drillDown() throws Exception {
-		/*setSelected(!getSelected());
+		setSelected(!getSelected());
+		
 		if (!selected) {
 			DeptList deptList = new DeptList();
 			deptList.setId(this.getPartCode());
@@ -331,7 +332,7 @@ public class Dept extends Database<IDept> implements IDept {
 				
 				setDeptEmployee(employeeList);
 			}
-		}*/
+		}
 	}
 		
 	public Object[] loadDeptList() throws Exception {
