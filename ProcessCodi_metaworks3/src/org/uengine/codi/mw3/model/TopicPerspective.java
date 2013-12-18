185617c40ec931e1d375eb038e21fa258309777d
somehow <somehow@uengine.org>
2013-12-18 12:51:03 +0000

    새주제 시 팝업을 스틱 모드로 변경

--- ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/TopicPerspective.java
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/TopicPerspective.java
@@ -1,10 +1,9 @@
 package org.uengine.codi.mw3.model;
 
 import org.metaworks.MetaworksContext;
-import org.metaworks.annotation.Face;
+import org.metaworks.ServiceMethodContext;
 import org.metaworks.annotation.ServiceMethod;
 import org.metaworks.website.MetaworksFile;
-import org.metaworks.widget.ModalWindow;
 import org.uengine.codi.mw3.knowledge.TopicPanel;
 import org.uengine.codi.mw3.knowledge.TopicTitle;
 
@@ -29,14 +28,15 @@ public class TopicPerspective extends Perspective {
 			this.topicMap = topicMap;
 		}
 	
-	@ServiceMethod(target="popup")
-	public ModalWindow addTopic() throws Exception{
+	@ServiceMethod(target=ServiceMethodContext.TARGET_STICK)
+	public Popup addTopic() throws Exception{
 		TopicTitle topicTitle = new TopicTitle();
 		topicTitle.setMetaworksContext(new MetaworksContext());
 		topicTitle.getMetaworksContext().setWhen(MetaworksContext.WHEN_NEW);
 		topicTitle.setLogoFile(new MetaworksFile());
 		topicTitle.session = session;
-		return new ModalWindow(topicTitle , 500, 270,  "$addTopic");
+		
+		return new Popup(500, 200, topicTitle);
 	}
 		
 }
