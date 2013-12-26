89ac1508b0f83612427395dde5afef4ef72d789d
cjsdus87@uengine.org <cjsdus87@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2013-12-26 01:00:51 +0000

    부서와 역할에 커서포인터와 개인영역 클릭 이벤트를 위해 로딩 없앰.

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/IDept.ejs.js
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/IDept.ejs.js
@@ -8,21 +8,3 @@ var org_uengine_codi_mw3_model_IDept = function(objectId, className){
 	this.object = mw3.objects[this.objectId];
 	
 };
-
-org_uengine_codi_mw3_model_IDept.prototype = {
-	startLoading : function(){
-		if(this.windowObjectId && mw3.getFaceHelper(this.windowObjectId) && mw3.getFaceHelper(this.windowObjectId).startLoading)
-			mw3.getFaceHelper(this.windowObjectId).startLoading();
-	},
-	endLoading : function(){
-		if(this.windowObjectId && mw3.getFaceHelper(this.windowObjectId) && mw3.getFaceHelper(this.windowObjectId).endLoading)
-			mw3.getFaceHelper(this.windowObjectId).endLoading();
-	},
-	destroy : function(){
-		if(this.windowObjectId && mw3.getFaceHelper(this.windowObjectId) && mw3.getFaceHelper(this.windowObjectId).endLoading)
-			mw3.getFaceHelper(this.windowObjectId).endLoading();
-	},
-	showStatus : function(message){
-
-	}
-};
--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/IRole.ejs.js
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/IRole.ejs.js
@@ -7,21 +7,3 @@ var org_uengine_codi_mw3_model_IRole = function(objectId, className){
 	this.object = mw3.objects[this.objectId];
 	
 };
-
-org_uengine_codi_mw3_model_IRole.prototype = {
-	startLoading : function(){
-		if(this.windowObjectId && mw3.getFaceHelper(this.windowObjectId) && mw3.getFaceHelper(this.windowObjectId).startLoading)
-			mw3.getFaceHelper(this.windowObjectId).startLoading();
-	},
-	endLoading : function(){
-		if(this.windowObjectId && mw3.getFaceHelper(this.windowObjectId) && mw3.getFaceHelper(this.windowObjectId).endLoading)
-			mw3.getFaceHelper(this.windowObjectId).endLoading();
-	},
-	destroy : function(){
-		if(this.windowObjectId && mw3.getFaceHelper(this.windowObjectId) && mw3.getFaceHelper(this.windowObjectId).endLoading)
-			mw3.getFaceHelper(this.windowObjectId).endLoading();
-	},
-	showStatus : function(message){
-	
-	}
-};
