347a091936633dc94412901b39ee1f853d658251
cjsdus87 <cjsdus87@uengine.org>
2013-12-26 01:16:57 +0000

    개인영역에 문서, 그룹, 부서도 클릭 이벤트 (파란 배경셀렉트 되게) 발생되게함.

--- ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/DocumentPerspective.ejs.js
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/DocumentPerspective.ejs.js
@@ -4,3 +4,10 @@ var org_uengine_codi_mw3_model_DocumentPerspective = function(objectId, classNam
 	this.className = className;
 	this.divId = 'objDiv_'+ this.objectId;
 };
+
+org_uengine_codi_mw3_model_DocumentPerspective.prototype.loaded = function(){
+	$('#' + this.divId + ' a').click(function(){
+		$('#navigator .depth2 .fist_menu li').removeClass('selected_navi');
+		$(this).parent().addClass('selected_navi');
+	});
+};
