fb163ab50121860860b71859a2c16b6667bfb017
sleepphoenix@uengine.org <sleepphoenix@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2014-01-08 09:10:21 +0000

    민수환씨, 류승호매니저 작업해야할 대상들 'TODO:'

--- trunk/ProcessCodi_metaworks3/WebContent/scripts/opengraph/OpenGraph-0.1-SNAPSHOT.js
+++ trunk/ProcessCodi_metaworks3/WebContent/scripts/opengraph/OpenGraph-0.1-SNAPSHOT.js
@@ -10233,6 +10233,12 @@ OG.shape.GroupShape.superclass = OG.shape.IShape;
 OG.shape.GroupShape.prototype.constructor = OG.shape.GroupShape;
 OG.GroupShape = OG.shape.GroupShape;
 
+
+OG.shape.GroupShape.prototype.layoutChild = function () {
+	//NONE
+	
+}
+
 /**
  * 드로잉할 Shape 을 생성하여 반환한다.
  *
@@ -10506,10 +10512,9 @@ OG.shape.bpmn.A_Task = function (label) {
 	//this.HaveButton = true;
 	this.LoopType = "None";
 	this.TaskType = "None";
-	this.Events = [];
 };
-OG.shape.bpmn.A_Task.prototype = new OG.shape.GroupShape();
-OG.shape.bpmn.A_Task.superclass = OG.shape.GroupShape;
+OG.shape.bpmn.A_Task.prototype = new OG.shape.GeomShape();
+OG.shape.bpmn.A_Task.superclass = OG.shape.GeomShape;
 OG.shape.bpmn.A_Task.prototype.constructor = OG.shape.bpmn.A_Task;
 OG.A_Task = OG.shape.bpmn.A_Task;
 
@@ -12893,13 +12898,30 @@ OG.shape.bpmn.M_Annotation.prototype.createShape = function () {
 OG.shape.bpmn.M_Group = function (label) {
 	OG.shape.bpmn.M_Group.superclass.call(this);
 
+	
+	this.CONNECTABLE = true;
+
+
 	this.SHAPE_ID = 'OG.shape.bpmn.M_Group';
 	this.label = label;
 };
-OG.shape.bpmn.M_Group.prototype = new OG.shape.GeomShape();
-OG.shape.bpmn.M_Group.superclass = OG.shape.GeomShape;
+
+
+
+OG.shape.bpmn.M_Group.prototype = new OG.shape.GroupShape();
+OG.shape.bpmn.M_Group.superclass = OG.shape.GroupShape;
 OG.shape.bpmn.M_Group.prototype.constructor = OG.shape.bpmn.M_Group;
 OG.M_Group = OG.shape.bpmn.M_Group;
+
+
+OG.shape.GroupShape.prototype.layoutChild = function () {
+	for(var event in this.Events){
+		//TODO: 
+		//var shapeOfEvent = event.shape;
+		//shapeOfEvent.x = .... ;
+	}
+}
+
 
 /**
  * 드로잉할 Shape 을 생성하여 반환한다.
@@ -12915,11 +12937,52 @@ OG.shape.bpmn.M_Group.prototype.createShape = function () {
 	this.geom = new OG.geometry.Rectangle([0, 0], 100, 100);
 	this.geom.style = new OG.geometry.Style({
 		'stroke-dasharray': '-',
-		"r"               : 6
-	});
+		"r"               : 6,
+		fill : 'none'     
 
+	});
+	
 	return this.geom;
 };
+
+OG.shape.bpmn.M_Group.prototype.createTerminal = function(){
+    if (!this.geom) {
+    		return [];
+    	}
+
+    var envelope = this.geom.getBoundary();
+
+    return [
+		new OG.Terminal(envelope.getCentroid(), OG.Constants.TERMINAL_TYPE.C, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(envelope.getRightCenter(), OG.Constants.TERMINAL_TYPE.E, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(envelope.getLeftCenter(), OG.Constants.TERMINAL_TYPE.W, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(envelope.getLowerCenter(), OG.Constants.TERMINAL_TYPE.S, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(envelope.getUpperCenter(), OG.Constants.TERMINAL_TYPE.N, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(envelope.getUpperLeft(), OG.Constants.TERMINAL_TYPE.N, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(envelope.getUpperRight(), OG.Constants.TERMINAL_TYPE.N, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(envelope.getLowerLeft(), OG.Constants.TERMINAL_TYPE.S, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(envelope.getLowerRight(), OG.Constants.TERMINAL_TYPE.S, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate((((envelope.getUpperCenter().x - envelope.getUpperLeft().x) / 3) * 1) + envelope.getUpperLeft().x, envelope.getUpperLeft().y), OG.Constants.TERMINAL_TYPE.N, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate((((envelope.getUpperCenter().x - envelope.getUpperLeft().x) / 3) * 2) + envelope.getUpperLeft().x, envelope.getUpperLeft().y), OG.Constants.TERMINAL_TYPE.N, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate((((envelope.getUpperRight().x - envelope.getUpperCenter().x) / 3) * 1) + envelope.getUpperCenter().x, envelope.getUpperLeft().y), OG.Constants.TERMINAL_TYPE.N, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate((((envelope.getUpperRight().x - envelope.getUpperCenter().x) / 3) * 2) + envelope.getUpperCenter().x, envelope.getUpperLeft().y), OG.Constants.TERMINAL_TYPE.N, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate((((envelope.getLowerCenter().x - envelope.getLowerLeft().x) / 3) * 1) + envelope.getLowerLeft().x, envelope.getLowerLeft().y), OG.Constants.TERMINAL_TYPE.S, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate((((envelope.getLowerCenter().x - envelope.getLowerLeft().x) / 3) * 2) + envelope.getLowerLeft().x, envelope.getLowerLeft().y), OG.Constants.TERMINAL_TYPE.S, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate((((envelope.getLowerRight().x - envelope.getLowerCenter().x) / 3) * 1) + envelope.getLowerCenter().x, envelope.getLowerLeft().y), OG.Constants.TERMINAL_TYPE.S, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate((((envelope.getLowerRight().x - envelope.getLowerCenter().x) / 3) * 2) + envelope.getLowerCenter().x, envelope.getLowerLeft().y), OG.Constants.TERMINAL_TYPE.S, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate( envelope.getUpperLeft().x, (((envelope.getLeftCenter().y - envelope.getUpperLeft().y) / 3) * 1) + envelope.getUpperLeft().y), OG.Constants.TERMINAL_TYPE.W, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate( envelope.getUpperLeft().x, (((envelope.getLeftCenter().y - envelope.getUpperLeft().y) / 3) * 2) + envelope.getUpperLeft().y), OG.Constants.TERMINAL_TYPE.W, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate( envelope.getUpperLeft().x, (((envelope.getLowerRight().y - envelope.getLeftCenter().y) / 3) * 1) + envelope.getLeftCenter().y), OG.Constants.TERMINAL_TYPE.W, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate( envelope.getUpperLeft().x, (((envelope.getLowerRight().y - envelope.getLeftCenter().y) / 3) * 2) + envelope.getLeftCenter().y), OG.Constants.TERMINAL_TYPE.W, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate( envelope.getUpperRight().x, (((envelope.getRightCenter().y - envelope.getUpperRight().y) / 3) * 1) + envelope.getUpperRight().y), OG.Constants.TERMINAL_TYPE.E, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate( envelope.getUpperRight().x, (((envelope.getRightCenter().y - envelope.getUpperRight().y) / 3) * 2) + envelope.getUpperRight().y), OG.Constants.TERMINAL_TYPE.E, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate( envelope.getUpperRight().x, (((envelope.getLowerRight().y - envelope.getRightCenter().y) / 3) * 1) + envelope.getRightCenter().y), OG.Constants.TERMINAL_TYPE.E, OG.Constants.TERMINAL_TYPE.INOUT),
+		new OG.Terminal(new OG.geometry.Coordinate( envelope.getUpperRight().x, (((envelope.getLowerRight().y - envelope.getRightCenter().y) / 3) * 2) + envelope.getRightCenter().y), OG.Constants.TERMINAL_TYPE.E, OG.Constants.TERMINAL_TYPE.INOUT)
+    ];
+}
+
+
+
 /**
  * BPMN : Text Shape
  *
@@ -18162,6 +18225,14 @@ OG.renderer.RaphaelRenderer.prototype.resize = function (element, offset, exclud
 		bBox, offsetX, offsetY, width, height, hRate, vRate;
 
 	this.removeCollapseGuide(element);
+	
+	//TODO:
+//	if (element.shape.SHAPE_TYPE == OG.Contraints.SHAPE_TYPE.GROUP){
+	//	element.shape.layoutChild();
+//	}
+    //END
+	
+	
 	if (rElement && type && geometry) {
 		geometry.resize(offset[0], offset[1], offset[2], offset[3]);
 
