a1079273338e78d26efd06a71fdbf83822ae6771
rucius@uengine.org <rucius@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2014-01-16 08:20:06 +0000

    리턴오브젝트어레이 투 리프레쉬

--- trunk/mw3/src/org/metaworks/common/MetaworksUtil.java
+++ trunk/mw3/src/org/metaworks/common/MetaworksUtil.java
@@ -3,6 +3,8 @@ package org.metaworks.common;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.metaworks.Refresh;
+
 public class MetaworksUtil {
 
 	static public void copyStream(InputStream sourceInputStream, OutputStream targetOutputStream) throws Exception{
@@ -20,5 +22,25 @@ public class MetaworksUtil {
 			if (sourceInputStream != null) try { sourceInputStream.close(); } catch (Exception e) {}
 			if (targetOutputStream != null) try { targetOutputStream.close(); } catch (Exception e) {}
 		}
-	}	
+	}
+	
+	static public Object[] putObjectArray(Object[] array, Object object){
+		return MetaworksUtil.putObjectArray(array,  object, true);
+	}
+	
+	// TODO: refectoring
+	static public Object[] putObjectArray(Object[] array, Object object, boolean useRefresh){
+		Object[] returnObject = new Object[ array.length + 1];
+
+		for (int i = 0; i < array.length; i++) {
+			if(useRefresh)
+				returnObject[i] = new Refresh(array[i]);
+			else
+				returnObject[i] = array[i];
+		}
+		returnObject[returnObject.length - 1] = object;
+		
+		
+		return returnObject;
+	}
 }
