acfbd6ff6471e197bcad4b389be1d84ae0776c55
sanaloveyou <sanaloveyou@uengine.org>
2014-01-20 07:52:49 +0000

    댓글삭제시 인스턴스 리스트에도 반영되도록 수정 댓글변경시 각 객체에서 처리하도록 return 부분 수정

--- ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/OverlayCommentWorkItem.java
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/OverlayCommentWorkItem.java
@@ -1,5 +1,6 @@
 package org.uengine.codi.mw3.model;
 
+import org.metaworks.ToAppend;
 import org.metaworks.annotation.Hidden;
 
 public class OverlayCommentWorkItem extends WorkItem{
@@ -13,5 +14,23 @@ public class OverlayCommentWorkItem extends WorkItem{
 	@Hidden//(on=false)
 	public OverlayCommentWorkItem comment() throws Exception {	
 		return null;
+	}
+	
+	public Object[] add() throws Exception {
+		boolean isOwnReturn = false;
+		if( WHEN_EDIT.equals(getMetaworksContext().getWhen())){
+			isOwnReturn = true;
+		}else{
+			isOwnReturn = false;
+		}
+		
+		Object[] returnObjects = super.add();
+		if( !isOwnReturn ){
+			WorkItem parentWorkItem = new WorkItem();
+			parentWorkItem.setTaskId(getOverlayCommentOption().getParentTaskId());
+			
+			returnObjects = new Object[]{new ToAppend(parentWorkItem, this)};
+		}
+		return returnObjects;
 	}
 }
