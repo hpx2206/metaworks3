978bdb544f400d9ae858fc1edf2e265eb2774c8f
cjsdus87 <cjsdus87@uengine.org>
2014-01-27 07:48:50 +0000

    프로필 수정시 "패스워크 재입력" 미입력시 수정안되도록 처리
    @Validator(name=ValidatorContext.VALIDATE_CONDITION, options="(password != confirmPassword)", message="패스워드와 동일하게 다시 입력해 주세요.")

--- ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/IEmployee.java
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/IEmployee.java
@@ -72,10 +72,7 @@ public interface IEmployee extends IDAO {
 	@Face(options="type", values="password")
 	@NonLoadable
 	@NonSavable
-//	@ValidatorSet({
-//		@Validator(name=ValidatorContext.VALIDATE_NOTNULL, message="비밀번호 확인를 입력하여 주십시오."),
-//		@Validator(name=ValidatorContext.VALIDATE_CONDITION, options={"password != confirmPassword"}, message="비밀번호 확인을 잘 못 입력하셨습니다. 다시 확인해주십시오.")
-//	})
+	@Validator(name=ValidatorContext.VALIDATE_CONDITION, options="(password != confirmPassword)", message="패스워드와 동일하게 다시 입력해 주세요.")
 	public String getConfirmPassword();
 	public void setConfirmPassword(String confirmPassword);
 	
