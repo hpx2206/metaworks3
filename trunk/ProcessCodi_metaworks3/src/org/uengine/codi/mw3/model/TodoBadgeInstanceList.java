ad3732ae97eadcabb4f564d213780962d83d97cc
sanaloveyou@uengine.org <sanaloveyou@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2014-01-27 08:25:02 +0000

    TodoBadge 를 다시 팝업으로 재설정

---
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/TodoBadgeInstanceList.java
@@ -0,0 +1,116 @@
+package org.uengine.codi.mw3.model;
+
+import org.directwebremoting.Browser;
+import org.directwebremoting.ScriptSessions;
+import org.metaworks.ContextAware;
+import org.metaworks.MetaworksContext;
+import org.metaworks.annotation.AutowiredToClient;
+import org.metaworks.annotation.Face;
+import org.metaworks.annotation.Hidden;
+import org.metaworks.annotation.Id;
+import org.metaworks.annotation.ServiceMethod;
+import org.metaworks.dao.MetaworksDAO;
+import org.uengine.codi.mw3.Login;
+
+@Face(ejsPath="dwr/metaworks/org/uengine/codi/mw3/model/InstanceList.ejs")
+public class TodoBadgeInstanceList implements ContextAware{
+
+	int page;
+		@Id
+		@Hidden		
+		public int getPage() {
+			return page;
+		}
+		public void setPage(int page) {
+			this.page = page;
+		}
+	
+	MetaworksContext metaworksContext;
+		public MetaworksContext getMetaworksContext() {
+			return metaworksContext;
+		}
+		public void setMetaworksContext(MetaworksContext metaworksContext) {
+			this.metaworksContext = metaworksContext;
+		}
+	ITodoBadgeInstance instances;
+		public ITodoBadgeInstance getInstances() {
+			return instances;
+		}
+		public void setInstances(ITodoBadgeInstance instances) {
+			this.instances = instances;
+		}
+
+	TodoBadgeInstanceList moreInstanceList;
+		public TodoBadgeInstanceList getMoreInstanceList() {
+			return moreInstanceList;
+		}
+		public void setMoreInstanceList(TodoBadgeInstanceList moreInstanceList) {
+			this.moreInstanceList = moreInstanceList;
+		}
+	Navigation navigation;
+		public Navigation getNavigation() {
+			return navigation;
+		}
+		public void setNavigation(Navigation navigation) {
+			this.navigation = navigation;
+		}
+
+	@AutowiredToClient
+	public Session session;
+	
+	public TodoBadgeInstanceList(){
+		this(null);
+	}
+	
+	public TodoBadgeInstanceList(Session session){
+		this.session = session;
+		
+		this.setNavigation(new Navigation(session));
+		this.setPage(1);
+	}
+		
+	public TodoBadgeInstanceList load() throws Exception {
+		return load(this.getNavigation());
+	}
+	
+	public TodoBadgeInstanceList load(Navigation navigation) throws Exception {
+		if(navigation.getPerspectiveType() != null && navigation.getPerspectiveType().equals("inbox")){			
+			//NEW WAY IS GOOD
+			Browser.withSession(Login.getSessionIdWithUserId(navigation.getEmployee().getEmpCode()), new Runnable(){
+				@Override
+				public void run() {
+					ScriptSessions.addFunctionCall("mw3.getAutowiredObject('" + TodoBadge.class.getName() + "').refresh", new Object[]{});					
+				}
+				
+			});
+		}
+		
+		int count =  ("phone".equals(navigation.getMedia())?InstanceList.PAGE_CNT_MOBILE:InstanceList.PAGE_CNT);
+		
+		IInstance instanceContents = Instance.load(navigation, getPage()-1, count);
+		
+		ITodoBadgeInstance todoBadgeIInstanceRef = (ITodoBadgeInstance)MetaworksDAO.createDAOImpl(ITodoBadgeInstance.class);
+		
+		if( instanceContents.size() > 0 ){
+			while(instanceContents.next()){
+				todoBadgeIInstanceRef.moveToInsertRow();
+				todoBadgeIInstanceRef.getImplementationObject().copyFrom(instanceContents);
+			}
+		}
+		todoBadgeIInstanceRef.setMetaworksContext(new MetaworksContext());
+		this.setInstances(todoBadgeIInstanceRef);
+		
+		// setting moreInstanceList
+		if( instanceContents.size() >= InstanceList.PAGE_CNT){
+			setMoreInstanceList(new TodoBadgeInstanceList());
+			getMoreInstanceList().setNavigation(navigation);
+			getMoreInstanceList().setPage(getPage()+1);
+		}
+		return this;
+		
+	}
+	@ServiceMethod(callByContent = true, except = { "instances" })
+	public void more() throws Exception {
+		load(this.getNavigation());
+	}
+}
