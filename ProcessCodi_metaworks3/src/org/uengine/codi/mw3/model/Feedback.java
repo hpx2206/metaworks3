ff8d4bd730e0f596b903256cb9a14580354b1d61
cjsdus87 <cjsdus87@uengine.org>
2014-01-28 11:30:36 +0000

    피드백 연결하여 모달로 띄우기

--- ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/Feedback.java
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/Feedback.java
@@ -1,5 +1,15 @@
 package org.uengine.codi.mw3.model;
 
+import org.metaworks.annotation.ServiceMethod;
+import org.metaworks.widget.ModalWindow;
+
+
 public class Feedback {
+	
+	@ServiceMethod
+	public ModalWindow popupFeedback(){
+		return new ModalWindow(new ContactUs(),800,550,"피드백");
+	}
+
 
 }
