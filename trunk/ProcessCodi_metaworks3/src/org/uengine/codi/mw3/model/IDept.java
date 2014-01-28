b7e2ae1c1cbc3cc7a2676ad2478b8da3208fb3b8
idtndhksha@uengine.org <idtndhksha@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2014-01-28 07:44:25 +0000

    \ 로 주제. 역할, 그룹 생성 벨리데이션

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/IDept.java
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/IDept.java
@@ -33,7 +33,7 @@ public interface IDept extends IDAO {
 	@ValidatorSet({
 		@Validator(name=ValidatorContext.VALIDATE_NOTNULL, message="부서 이름을 입력해주세요."),
 		@Validator(name=ValidatorContext.VALIDATE_MAX , options={"20"}, message="20자 이내로 입력해주세요."),
-		@Validator(name=ValidatorContext.VALIDATE_REGULAREXPRESSION, options={"/^[^~!@\\#$%^&*\\()\\-=+_\'\"]+$/"}, message="다음과 같은 문자는 입력 할 수 없습니다. ~!@#$%^&*()\\-=+_\'\"")
+		@Validator(name=ValidatorContext.VALIDATE_REGULAREXPRESSION, options={"/^[^~!@\\\\#$%^&*\\()\\-=+_\'\"]+$/"}, message="다음과 같은 문자는 입력 할 수 없습니다. ~!@#$%^&*()\\-=+_\'\"")
 	})
 	public String getPartName();
 	public void setPartName(String partName);
