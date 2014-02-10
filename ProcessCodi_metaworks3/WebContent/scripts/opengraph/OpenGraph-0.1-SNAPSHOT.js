4aea57df9e6e5e351ce19ba96f0d88b8c781fa59
grayspec <grayspec@uengine.org>
2014-02-10 07:35:41 +0000

    toJSON - add attribute Cell @childs  ; 자식노드 속성추가  + 연결선(OG.shape.EdgeShape) 제외 (원복)

--- ProcessCodi_metaworks3/WebContent/scripts/opengraph/OpenGraph-0.1-SNAPSHOT.js
+++ ProcessCodi_metaworks3/WebContent/scripts/opengraph/OpenGraph-0.1-SNAPSHOT.js
@@ -11333,53 +11333,6 @@ OG.shape.bpmn.E_End.prototype.createTerminal = function () {
 	];
 };
 
-OG.shape.bpmn.E_End2 = function (label) {
-	OG.shape.bpmn.E_End2.superclass.call(this);
-
-	this.SHAPE_ID = 'OG.shape.bpmn.E_End2';
-	this.label = label;
-};
-OG.shape.bpmn.E_End2.prototype = new OG.shape.GeomShape();
-OG.shape.bpmn.E_End2.superclass = OG.shape.GeomShape;
-OG.shape.bpmn.E_End2.prototype.constructor = OG.shape.bpmn.E_End2;
-OG.E_End = OG.shape.bpmn.E_End2;
-
-/**
- * 드로잉할 Shape 을 생성하여 반환한다.
- *
- * @return {OG.geometry.Geometry} Shape 정보
- * @override
- */
-
-OG.shape.bpmn.E_End2.prototype.createShape = function () {
-
-};
-
-
-
-/**
- * Shape 간의 연결을 위한 Terminal 을 반환한다.
- *
- * @return {OG.Terminal[]} Terminal
- * @override
- */
-OG.shape.bpmn.E_End2.prototype.createTerminal = function () {
-	if (!this.geom) {
-		return [];
-	}
-
-	var envelope = this.geom.getBoundary();
-
-	return [
-		new OG.Terminal(envelope.getCentroid(), OG.Constants.TERMINAL_TYPE.C, OG.Constants.TERMINAL_TYPE.IN),
-		new OG.Terminal(envelope.getRightCenter(), OG.Constants.TERMINAL_TYPE.E, OG.Constants.TERMINAL_TYPE.IN),
-		new OG.Terminal(envelope.getLeftCenter(), OG.Constants.TERMINAL_TYPE.W, OG.Constants.TERMINAL_TYPE.IN),
-		new OG.Terminal(envelope.getLowerCenter(), OG.Constants.TERMINAL_TYPE.S, OG.Constants.TERMINAL_TYPE.IN),
-		new OG.Terminal(envelope.getUpperCenter(), OG.Constants.TERMINAL_TYPE.N, OG.Constants.TERMINAL_TYPE.IN)
-	];
-};
-
-
 /**
  * BPMN : Cancel End Event Shape
  *
@@ -24013,9 +23966,9 @@ OG.graph.Canvas.prototype = {
 					}else{
 						cellMap[$(childNode).attr('id')]['@parent'] = groupId;	
 						
-						if(!(childNode.shape instanceof OG.shape.EdgeShape)){
+						//if(!(childNode.shape instanceof OG.shape.EdgeShape)){
 							cellMap[groupId]['@childs'].push($(childNode).attr('id'));
-						}
+						//}
 					}
 				}				
 			} //end of for
