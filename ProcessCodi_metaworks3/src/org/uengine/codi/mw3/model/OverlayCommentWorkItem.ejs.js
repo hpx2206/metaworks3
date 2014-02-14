dee07e846740ba37a7de8efa330825318c342ff0
somehow <somehow@uengine.org>
2014-02-14 08:34:01 +0000

    덧글 모드 이상 수정

--- ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/OverlayCommentWorkItem.ejs.js
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/OverlayCommentWorkItem.ejs.js
@@ -1,12 +1,16 @@
 var org_uengine_codi_mw3_model_OverlayCommentWorkItem = function(objectId, className){
 	
 	this.objectId = objectId;
-
-	var workItem = mw3.objects[this.objectId];
-	var parentWorkItem = mw3.getAutowiredObject("org.uengine.codi.mw3.model.IWorkItem@"+ workItem.overlayCommentOption.parentTaskId);
+	this.objectDivId = mw3._getObjectDivId(this.objectId);
+	this.objectDiv = $('#' + this.objectDivId);
+	
+	this.object = mw3.objects[this.objectId];
+	
+	var parentWorkItem = mw3.getAutowiredObject("org.uengine.codi.mw3.model.IWorkItem@"+ this.object.overlayCommentOption.parentTaskId);
+	var div = $("#objDiv_" + parentWorkItem.__objectId);
 	
 	if('view' == mw3.objectContexts[this.objectId].__metaworksContext.when){
-		if('comment' == workItem.metaworksContext.how){
+		if('comment' == this.object.metaworksContext.how){
 			$('.overlay_comment').hover(
 					function(){$(this).css({"z-index":"101",'opacity':'1'})},
 					function(){$(this).css({"z-index":"100",'opacity':'0.5'})}
@@ -16,41 +20,59 @@ var org_uengine_codi_mw3_model_OverlayCommentWorkItem = function(objectId, class
 					function(){$(this).parent().find('.overlay_comment').css({'z-index':'100','opacity':'0.5'})}
 			);
 		}else{
-			var clone = mw3.clone(workItem);
-			clone.metaworksContext.how = 'comment';
-	
-			mw3.removeObject(this.objectId);
-			
-			var ovryCmntTarget = {
-		    	__className : 'org.uengine.codi.mw3.model.IWorkItem',
-		    	taskId : workItem.overlayCommentOption.parentTaskId
-		    };
-		    
-			var toAppend = {
-				__className : 'org.metaworks.ToAppend',
-				parent : ovryCmntTarget,
-				target : clone,
-				match : true
-			};
-			
-			mw3.locateObject(toAppend, toAppend.__className, 'body');
+			if(div.length){
+				var faceHelper = this;
+				
+				faceHelper.draw();
+			}else{
+				this.windowObjectId = this.objectDiv.closest('.mw3_window').attr('objectId');
+				
+				$('#' + mw3._getObjectDivId(this.windowObjectId)).one(
+					'loaded.workitem_' + this.object.overlayCommentOption.parentTaskId, 
+					{objectId: this.objectId}, 
+					function(event, ui){
+						mw3.getFaceHelper(event.data.objectId).draw();
+				});
+			}
 		}
 	}else{
-		var div = $("#objDiv_" + parentWorkItem.__objectId);
-	
-		if(workItem.taskId==null){
-			workItem.overlayCommentOption.x = (mw3.mouseX - div.offset().left -22) / (div.width()/100);
-			workItem.overlayCommentOption.y = (mw3.mouseY - div.offset().top -34) / (div.height()/100);
+		if(this.object.taskId==null){
+			this.object.overlayCommentOption.x = (mw3.mouseX - div.offset().left -22) / (div.width()/100);
+			this.object.overlayCommentOption.y = (mw3.mouseY - div.offset().top -34) / (div.height()/100);
 		}		
 
-		var title = (workItem.metaworksContext.when == mw3.WHEN_EDIT) ? workItem.title :'';
+		var title = (this.object.metaworksContext.when == mw3.WHEN_EDIT) ? this.object.title :'';
 		var comment = prompt('설명을 입력해주세요 : ', title);
 		
 		if(comment){
-			workItem.rootInstId = parentWorkItem.rootInstId;
-			workItem.title = comment;
-			workItem.add();		
+			this.object.rootInstId = parentWorkItem.rootInstId;
+			this.object.title = comment;
+			this.object.add();		
 		}			
 	}	
 
 };
+
+org_uengine_codi_mw3_model_OverlayCommentWorkItem.prototype = {
+	draw : function(){
+		var clone = mw3.clone(this.object);
+		clone.metaworksContext.how = 'comment';
+
+		mw3.removeObject(this.objectId);
+		
+		var ovryCmntTarget = {
+	    	__className : 'org.uengine.codi.mw3.model.IWorkItem',
+	    	taskId : this.object.overlayCommentOption.parentTaskId
+	    };
+	    
+		var toAppend = {
+			__className : 'org.metaworks.ToAppend',
+			parent : ovryCmntTarget,
+			target : clone,
+			match : true
+		};
+		
+		mw3.locateObject(toAppend, toAppend.__className, 'body');
+		mw3.onLoadFaceHelperScript();
+	}		
+}
