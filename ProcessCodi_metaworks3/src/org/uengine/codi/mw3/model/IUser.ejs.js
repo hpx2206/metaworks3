377143d6a01257f738eee2a81826825ad704139d
somehow <somehow@uengine.org>
2014-02-14 04:57:30 +0000

    사용자 액션 시 사용자 얼굴에서 로딩아이콘 처리

--- ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/IUser.ejs.js
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/IUser.ejs.js
@@ -3,67 +3,23 @@ var org_uengine_codi_mw3_model_IUser = function(objectId, className){
 	this.className = className;
 	this.divId = mw3._getObjectDivId(this.objectId);
 	
-	this.windowObjectId = $('#' + this.divId).closest('.mw3_window').attr('objectId');
-	
 	var user = mw3.objects[objectId];
 	if(user == null)
 		return true;
-
 	
 	if(user.metaworksContext && user.metaworksContext.when=='contacts'){
 		var msg=$('#objDiv_' + objectId).find('.fontgray').text();
-		var comp=msg.length ;
-		
-		
-		var count=0 ;
+		var comp = msg.length;
+		var count = 0;
 		
 		var typing = function(){ 
-			if(count<=comp){ 
-				
+			if(count <= comp){ 
 				$('#objDiv_' + objectId).find('.fontgray').text(msg.substring(0,count));
 				count++ ;
 				setTimeout(function(){typing();}, 100); 
-				
 			}
 		};
 		
 		typing();
-		
-	}
-
-	
-};
-
-org_uengine_codi_mw3_model_IUser.prototype = {
-	destroy : function(){
-		this.endLoading();
-	},
-	startLoading : function(){
-		if(this.windowObjectId && mw3.getFaceHelper(this.windowObjectId) && mw3.getFaceHelper(this.windowObjectId).startLoading)
-			mw3.getFaceHelper(this.windowObjectId).startLoading();
-		else
-			mw3.startLoading(this.objectId);
-	},
-	endLoading : function(){
-		if(this.windowObjectId && mw3.getFaceHelper(this.windowObjectId) && mw3.getFaceHelper(this.windowObjectId).endLoading)
-			mw3.getFaceHelper(this.windowObjectId).endLoading();
-		else
-			mw3.endLoading(this.objectId);
-
-	},
-	showStatus : function(message){
-		if(this.windowObjectId && mw3.getFaceHelper(this.windowObjectId) && mw3.getFaceHelper(this.windowObjectId).endLoading)
-			mw3.getFaceHelper(this.windowObjectId).endLoading();
-		else
-			mw3.showInfo(this.objectId, message);
-	},
-	ableContextMenu: function(methodName) {
-		
-		if(methodName == 'removeContact') {
-			var object = mw3.objects[this.objectId];
-			if(object.metaworksContext.how != null && object.metaworksContext.how=='info')
-				return false;
-		}
-		return true;
 	}
 };
