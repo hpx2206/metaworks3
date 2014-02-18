2cecf6efbf4958b88d4268107ca1c5846aff2d34
cjsdus87 <cjsdus87@uengine.org>
2014-02-18 02:16:56 +0000

    uengine.properties에서 불러오던  url을 httpServletRequest에서 불러오기

--- ProcessCodi_metaworks3/src/org/uengine/cloud/saasfier/TenantContext.java
+++ ProcessCodi_metaworks3/src/org/uengine/cloud/saasfier/TenantContext.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.metaworks.dao.TransactionContext;
 import org.uengine.codi.mw3.StartCodi;
 import org.uengine.codi.util.CodiStringUtil;
@@ -42,53 +44,34 @@ public class TenantContext{
 	
 	public static String getURL(String tenantId) throws MalformedURLException {
 
-		/*
-		String requestedURL = TransactionContext.getThreadLocalInstance().getRequest().getRequestURL().toString(); 
-        String base = requestedURL.substring( 0, requestedURL.lastIndexOf( "/" ) );
-        
-        URL urlURL = new java.net.URL(base);
-       	String host = urlURL.getHost();
-       	int port = urlURL.getPort();
-       	String protocol = urlURL.getProtocol();
+		HttpServletRequest request = TransactionContext.getThreadLocalInstance().getRequest();
+		String url = request.getRequestURL().toString();
+		String codebase = url.substring( 0, url.lastIndexOf( "/" ) );
+		URL urlURL = new java.net.URL(codebase);
+		String host = urlURL.getHost();
+		int port = urlURL.getPort();
+		
+		String path = urlURL.getPath();
+		String contextOnly = path.substring(0, path.substring(1).indexOf("/")+1);
+		String protocol = urlURL.getProtocol();
 
-       	StringBuffer url = new StringBuffer();
-       	
-       	url.append(protocol + "://");
-       	if("1".equals(StartCodi.USE_MULTITENANCY)){
-       		url.append((tenantId==null?"":tenantId+"."));
-       	}
-       	
-       	url.append(host);
-		url.append((port == 80 ? "" : ":"+port));
-		url.append(TransactionContext.getThreadLocalInstance().getRequest().getContextPath());
-		*/
+		StringBuffer serverPath = new StringBuffer();
 		
-		String ip = GlobalContext.getPropertyString("web.server.ip", "uenginecloud.com");
-		String port = GlobalContext.getPropertyString("web.server.port", "7070");
-		String contextRoot = GlobalContext.getPropertyString("web.context.root", CodiStringUtil.URL_SEPARATOR);
-
-		if(!contextRoot.startsWith(CodiStringUtil.URL_SEPARATOR)){
-			contextRoot = CodiStringUtil.URL_SEPARATOR + contextRoot;
-		}
+		serverPath.append(protocol+"://");
 		
-		if(contextRoot.charAt(0) == '/'){
-			
-		}
 		
-		StringBuffer url = new StringBuffer();
-		url.append("http://");
-
-       	if("1".equals(StartCodi.USE_MULTITENANCY))
-       		url.append((tenantId==null?"":tenantId+"."));
-
-		url.append(ip);
+		if(host.length()>0)
+			serverPath.append(host);
 		
-		if(port.length() > 0)
-			url.append(":" + port);
-		if(contextRoot.length() > 0)
-			url.append(contextRoot);
+		if (port>-1)
+			serverPath.append(":"+urlURL.getPort());
 		
-		return CodiStringUtil.lastLastFileSeparatorChar(url.toString());
+		if(contextOnly.length()>0 && !contextOnly.equals("/dwr")) 
+			serverPath.append(contextOnly);
+		
+		System.out.println(CodiStringUtil.lastLastFileSeparatorChar(serverPath.toString()));
+				
+		return CodiStringUtil.lastLastFileSeparatorChar(serverPath.toString());
 	}
 	
 	public static TenantContext getThreadLocalInstance(){
