86bc64355badd8ea37fd31e90f5be43310d1b557
idtndhksha <idtndhksha@uengine.org>
2014-02-20 09:05:40 +0000

    기본주제에서 글을 쓰면,  오류 메시지 창이 뜸 해당 토픽 팔로워에 존재하는 유저를 디비상에서 강제로 삭제한 경우 나오는 에러 디비에서 삭제된 유저는 팔로워에 안보이게 처리

--- ProcessCodi_metaworks3/src/org/uengine/codi/mw3/knowledge/TopicMapping.java
+++ ProcessCodi_metaworks3/src/org/uengine/codi/mw3/knowledge/TopicMapping.java
@@ -169,9 +169,9 @@ public class TopicMapping extends Database<ITopicMapping> implements ITopicMappi
 		
 		StringBuffer sql = new StringBuffer();
 		sql.append("SELECT distinct tm.userId endpoint, ifnull(e.empname,tm.userName) resname, tm.assigntype, tm.topicId parentId, '" + Follower.TYPE_TOPIC + "' parentType");
-		sql.append("  FROM bpm_topicmapping tm");
-		sql.append("  LEFT JOIN emptable e on tm.userId = e.empcode and tm.assigntype = 0");
-		sql.append(" WHERE tm.topicid = ?topicId");
+		sql.append("  FROM bpm_topicmapping tm,  emptable e");
+		sql.append(" WHERE tm.userId = e.empcode and tm.assigntype = 0");
+		sql.append("   AND tm.topicid = ?topicId");
 		
 		IFollower follower = (IFollower) Database.sql(IFollower.class, sql.toString());
 		follower.set("topicId", this.getTopicId());
