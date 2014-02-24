4cd17f2689e6b99dda34b269a466277f3868418f
somehow@uengine.org <somehow@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2014-02-24 03:08:43 +0000

    덧글 위치가 입력 안되는 현상 수정

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/OverlayCommentWorkItem.java
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/OverlayCommentWorkItem.java
@@ -33,6 +33,8 @@ public class OverlayCommentWorkItem extends WorkItem{
 		instance.setInstId(parentWorkItem.databaseMe().getRootInstId());
 		
 		this.setRootInstId(instance.databaseMe().getRootInstId());
+		this.setExt1(getOverlayCommentOption().getX());
+		this.setExt2(getOverlayCommentOption().getY());
 		
 		/*
 		 * fileWorkItem 은 GenericWorkItem 이 한번 감싸고, 다른 instId 를 가지고 따로 그려진다.
