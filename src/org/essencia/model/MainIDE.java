1d71867ed9a901d7b167636fe4c924071fe4fa91
iklim <iklim@uengine.org>
2014-06-13 07:51:05 +0000

    탭패널 관련 커밋

--- src/org/essencia/model/MainIDE.java
+++ src/org/essencia/model/MainIDE.java
@@ -1,7 +1,10 @@
 package org.essencia.model;
+
 import org.essencia.ide.Navigator;
 import org.metaworks.annotation.ServiceMethod;
 import org.metaworks.example.ide.Workspace;
+import org.metaworks.widget.Tab;
+import org.metaworks.widget.TabPanel;
 import org.metaworks.widget.layout.Layout;
 
 
@@ -30,22 +33,50 @@ public class MainIDE {
 	@ServiceMethod(callByContent=true)
 	public MainPanel load(){
 		
-		
 		Navigator navigator = new Navigator();		
 		navigator.load("CodeBase", "EssenciaWork");
 		
-		MainWindow navigatorWindow = new MainWindow(null,NAVIGATOR);
+		MainWindow navigatorWindow = new MainWindow(null, NAVIGATOR);
 		navigatorWindow.setPanel(navigator);
-		MainWindow editorWindow = new MainWindow(null,EDITOR);
+		MainWindow editorWindow = new MainWindow(null, EDITOR);
 		
-		Layout layout = new Layout();
+		TabPanel tp = new TabPanel();
+
+		Tab t = new Tab("tab");
+		
+		t.setTabContent("탭컨탠츠");
+		tp.addTab(t);
+
+		Tab t1 = new Tab("tab1");
+		t1.setTabContent("탭컨탠츠1");
+		tp.addTab(t1);
+		
+		TabPanel tp2 = new TabPanel();
+		
+		Tab t2 = new Tab("2");
+		t2.setTabContent("탭컨탠츠2");
+		tp2.addTab(t2);
+		
+		
+		Tab t3 = new Tab("3");
+		t3.setTabContent("탭컨탠츠3");
+		tp2.addTab(t3);
+		
+		Layout innerLayout = new Layout();
+		innerLayout.setCenter(tp);
+		innerLayout.setSouth(tp2);
+		editorWindow.setPanel(innerLayout);
+		
+		layout = new Layout();
 		layout.setId("main");
 		layout.setName("main");
-		layout.setCenter(editorWindow);
 		layout.setWest(navigatorWindow);
+		layout.setCenter(editorWindow);
 		layout.setOptions("togglerLength_open:0, spacing_open:0, spacing_closed:0, south__spacing_open:5, west__size:250");
 		
-		this.setLayout(layout);
-		return new MainPanel(this.getLayout());
+		MainPanel mainPanel = new MainPanel();
+		mainPanel.setPanel(getLayout());
+		
+		return mainPanel;
 	}
 }
