afeac4dc85c95fb24208ab5d325f4df623a068bb
idtndhksha@uengine.org <idtndhksha@uengine.org@4e09dde0-a587-f01d-54b8-73f29ba8ffd6>
2014-06-27 05:21:23 +0000

    더보기 시 설명 안달리는 이슈

--- trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/OverlayCommentWorkItem.ejs.js
+++ trunk/ProcessCodi_metaworks3/src/org/uengine/codi/mw3/model/OverlayCommentWorkItem.ejs.js
@@ -3,56 +3,23 @@ var org_uengine_codi_mw3_model_OverlayCommentWorkItem = function(objectId, class
 	this.objectId = objectId;
 	this.objectDivId = mw3._getObjectDivId(this.objectId);
 	this.objectDiv = $('#' + this.objectDivId);
-	
 	this.object = mw3.objects[this.objectId];
+
+	var faceHelper = this;
+	var context = mw3.objectContexts[this.objectId].__metaworksContext;
 	
-	var parentWorkItem = mw3.getAutowiredObject("org.uengine.codi.mw3.model.IWorkItem@"+ this.object.overlayCommentOption.parentTaskId);
-	var div = $("#objDiv_" + parentWorkItem.__objectId);
-	
-	if('view' == mw3.objectContexts[this.objectId].__metaworksContext.when){
-		if('comment' == this.object.metaworksContext.how){
-			$('.overlay_comment').hover(
-					function(){$(this).css({"z-index":"101",'opacity':'1'})},
-					function(){$(this).css({"z-index":"100",'opacity':'0.5'})}
-			);
-			$('.overlay_comment_line').hover(
-					function(){$(this).parent().find('.overlay_comment').css({'z-index':'101','opacity':'1'})},
-					function(){$(this).parent().find('.overlay_comment').css({'z-index':'100','opacity':'0.5'})}
-			);
+	if('view' == context.when){
+		if('comment' == context.how){
+			faceHelper.bindMouseOverHighlight();
 		}else{
-			if(div.length && mw3.MESSAGE_LOADING != div.html()){
-				var faceHelper = this;
-				
-				faceHelper.draw();
-			}else{
-				this.windowObjectId = this.objectDiv.closest('.mw3_window').attr('objectId');
-				
-				$('#' + mw3._getObjectDivId(this.windowObjectId)).one(
-					'loaded.workitem_' + this.object.overlayCommentOption.parentTaskId, 
-					{objectId: this.objectId}, 
-					function(event, ui){
-						mw3.getFaceHelper(event.data.objectId).draw();
-				});
-			}
+		 	if(faceHelper.ableDraw())
+		 		faceHelper.draw();
+			else
+				faceHelper.bind();
 		}
 	}else{
-		if(this.object.taskId==null){
-			var faceHelper = parentWorkItem.getFaceHelper();
-			this.object.overlayCommentOption.x = (faceHelper.mouseX - div.offset().left -10) / (div.width()/100);
-			this.object.overlayCommentOption.y = (faceHelper.mouseY - div.offset().top ) / (div.height()/100);
-
-		}		
-
-		var title = (this.object.metaworksContext.when == mw3.WHEN_EDIT) ? this.object.title :'';
-		var comment = prompt('설명을 입력해주세요 : ', title);
-		
-		if(comment){
-			this.object.rootInstId = parentWorkItem.rootInstId;
-			this.object.title = comment;
-			this.object.add();		
-		}			
+		faceHelper.showInputDialog();
 	}	
-
 };
 
 org_uengine_codi_mw3_model_OverlayCommentWorkItem.prototype = {
@@ -74,5 +41,58 @@ org_uengine_codi_mw3_model_OverlayCommentWorkItem.prototype = {
 		
 		mw3.locateObject(toAppend, toAppend.__className, 'body');
 		mw3.onLoadFaceHelperScript();
-	}		
+	},
+	
+	ableDraw : function(){
+		var parentWorkItem = mw3.getAutowiredObject("org.uengine.codi.mw3.model.IWorkItem@"+ this.object.overlayCommentOption.parentTaskId, true);
+
+		if(parentWorkItem){
+			var div = $("#objDiv_" + parentWorkItem.__objectId);
+			if(div.length && mw3.MESSAGE_LOADING != div.html() != parentWorkItem.more)
+				return true;
+		}
+		return false;
+	},
+	
+	bind : function(){
+		this.windowObjectId = this.objectDiv.closest('.mw3_window').attr('objectId');
+			
+		$('#' + mw3._getObjectDivId(this.windowObjectId)).one(
+			'loaded.workitem_' + this.object.overlayCommentOption.parentTaskId, 
+			{objectId: this.objectId}, 
+			function(event, ui){
+				mw3.getFaceHelper(event.data.objectId).draw();
+		});
+	},
+	
+	bindMouseOverHighlight : function(){
+		$('.overlay_comment').hover(
+				function(){$(this).css({"z-index":"101",'opacity':'1'})},
+				function(){$(this).css({"z-index":"100",'opacity':'0.5'})}
+		);
+		$('.overlay_comment_line').hover(
+				function(){$(this).parent().find('.overlay_comment').css({'z-index':'101','opacity':'1'})},
+				function(){$(this).parent().find('.overlay_comment').css({'z-index':'100','opacity':'0.5'})}
+		);
+	},	
+	
+	showInputDialog : function(){
+		var parentWorkItem = mw3.getAutowiredObject("org.uengine.codi.mw3.model.IWorkItem@"+ this.object.overlayCommentOption.parentTaskId, true);
+		var div = $("#objDiv_" + parentWorkItem.__objectId);
+		
+		if(this.object.taskId==null){
+			var faceHelper = parentWorkItem.getFaceHelper();
+			this.object.overlayCommentOption.x = (faceHelper.mouseX - div.offset().left -10) / (div.width()/100);
+			this.object.overlayCommentOption.y = (faceHelper.mouseY - div.offset().top ) / (div.height()/100);
+		}		
+
+		var title = (this.object.metaworksContext.when == mw3.WHEN_EDIT) ? this.object.title :'';
+		var comment = prompt('설명을 입력해주세요 : ', title);
+		
+		if(comment){
+			this.object.rootInstId = parentWorkItem.rootInstId;
+			this.object.title = comment;
+			this.object.add();		
+		}		
+	}
 }
